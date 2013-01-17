d0f0460f7c2422e4b82f9c7ad35a471387b7ccb4
rneder <reinhard.neder@krist.uni-erlangen.de>
2013-01-17 15:46:12 +0100

    Changed into allocatable arrays, created variables for internal storage

--- discus/prog/micro_mod.f90
+++ discus/prog/micro_mod.f90
@@ -6,18 +6,39 @@ USE config_mod
 !
 SAVE
 !
+INTEGER      :: MK_MAX_SCAT
+INTEGER      :: MK_MAX_ATOM
+!
 CHARACTER*80 mk_name
 CHARACTER*16 mk_spcgr
-CHARACTER*4  mk_at_lis(0:MK_MAX_SCAT)
+CHARACTER(LEN=4), DIMENSION(:), ALLOCATABLE ::  mk_at_lis  ! (0:MK_MAX_SCAT)
 !
-REAL         mk_dw(0:MK_MAX_SCAT)
-REAL         mk_a0(3)
-REAL         mk_win(3)
-REAL         mk_dim(3,2)
+REAL            , DIMENSION(:), ALLOCATABLE ::  mk_dw      ! (0:MK_MAX_SCAT)
+REAL                :: mk_a0(3)
+REAL                :: mk_win(3)
+REAL                :: mk_dim(3,2)
 !
-INTEGER*4    mk_nscat
-INTEGER      mk_natoms
+INTEGER             :: ik,il
+INTEGER*4           :: mk_nscat
+INTEGER             :: mk_natoms
 !
-INTEGER      mk_spcgr_ianz,mk_spcgr_para
+INTEGER             :: mk_spcgr_ianz,mk_spcgr_para
+INTEGER             :: mk_spcgr_no
+INTEGER, PARAMETER  ::  mk_GEN_ADD_MAX  =  14
+INTEGER, PARAMETER  ::  mk_SYM_ADD_MAX  =  14
+!
+INTEGER             ::  mk_gen_add_n                  = 0
+INTEGER             ::  mk_gen_add_power(mk_GEN_ADD_MAX) = 1
+REAL                ::  mk_gen_add(4,4,0:mk_GEN_ADD_MAX) = &
+         RESHAPE((/(1.,(0.,0.,0.,0.,1.,ik=1,3),il=0,mk_GEN_ADD_MAX)/),SHAPE(mk_gen_add ))
+!
+INTEGER             ::  mk_sym_add_n                  = 0
+INTEGER             ::  mk_sym_add_power(mk_SYM_ADD_MAX) = 1
+REAL                ::  mk_sym_add(4,4,0:mk_SYM_ADD_MAX) = &
+         RESHAPE((/(1.,(0.,0.,0.,0.,1.,ik=1,3),il=0,mk_SYM_ADD_MAX)/),SHAPE(mk_sym_add ))
+!
+LOGICAL             :: mk_infile_internal  ! File with domain atoms in stored internally
+INTEGER             :: mk_iatom            ! Current atom number in internal file
+INTEGER             :: mic_size_of
 !
 END MODULE micro_mod
