9aa3b81217dc2ab7215579db2501f0fdedaddbaf
Ross Whitfield <whitfieldre@ornl.gov>
2014-02-06 10:40:10 -0500

    Added support for CUDA 3D calculaion in four_strucf.

--- discus/prog/four_strucf_cuda.cu
+++ discus/prog/four_strucf_cuda.cu
@@ -9,15 +9,19 @@
 __global__ void init_array_kernel(cuFloatComplex*, int);
 
 __global__ void four_strucf_cuda_kernel(cuFloatComplex*, cuFloatComplex*,
-					int, int, int,
-					int, int, int);
+					int, int, int, int,
+					int, int, int, int);
+
+__global__ void four_strucf_cuda_kernel_2d(cuFloatComplex*, cuFloatComplex*,
+					   int, int, int, int,
+					   int, int, int, int);
 
 extern "C"{
   void four_strucf_cuda_(cuFloatComplex *tcsf, cuFloatComplex *cex, float *xat, int *nxat, int *num, float *xm, float *win, float *vin, float *uin, int *cr_natoms)
   {
     int nnum = num[0]*num[1]*num[2];
-    double xarg0, xincu, xincv;
-    int iarg0, iincu, iincv;
+    double xarg0, xincu, xincv, xincw;
+    int iarg0, iincu, iincv, iincw;
     int blockDim = 64;
     int gridDim = (nnum + blockDim - 1) / blockDim;
     
@@ -37,15 +41,17 @@ extern "C"{
       xarg0 = xm[0]  * xat[l] + xm[1]  * xat[l+cr_natoms[0]] + xm[2]  * xat[l+cr_natoms[0]*2];
       xincu = uin[0] * xat[l] + uin[1] * xat[l+cr_natoms[0]] + uin[2] * xat[l+cr_natoms[0]*2];
       xincv = vin[0] * xat[l] + vin[1] * xat[l+cr_natoms[0]] + vin[2] * xat[l+cr_natoms[0]*2];
+      xincw = win[0] * xat[l] + win[1] * xat[l+cr_natoms[0]] + win[2] * xat[l+cr_natoms[0]*2];
       //Convert to high precision integers (64*i2pi=2^20) ...
       iarg0 = (int) rint( 64 * I2PI * (xarg0 - (int) xarg0 + 1.) );
       iincu = (int) rint( 64 * I2PI * (xincu - (int) xincu + 1.) );
       iincv = (int) rint( 64 * I2PI * (xincv - (int) xincv + 1.) );
+      iincw = (int) rint( 64 * I2PI * (xincw - (int) xincw + 1.) );
       
       four_strucf_cuda_kernel<<<gridDim, blockDim>>>
 	(d_cex, d_tcsf,
-	 num[0],num[1],nnum,
-	 iarg0,iincu,iincv);
+	 num[0],num[1],num[2],nnum,
+	 iarg0,iincu,iincv,iincw);
     }
     
     cudaMemcpy(tcsf, d_tcsf, nnum*sizeof(cuFloatComplex), cudaMemcpyDeviceToHost);
@@ -57,8 +63,32 @@ extern "C"{
 }
 
 __global__ void four_strucf_cuda_kernel(cuFloatComplex* d_cex, cuFloatComplex* d_tcsf,
-					int num1, int num2, int nnum,
-					int iarg0, int iincu, int iincv)
+					int num1, int num2, int num3, int nnum,
+					int iarg0, int iincu, int iincv, int iincw)
+{
+  int i, j, k, iadd, iarg;
+  unsigned int idx = blockDim.x*blockIdx.x + threadIdx.x;
+  //Loop over all image pixels 'idx'. 'iadd' is the address of the
+  //argument to the complex exponent (in the table 'cex()'). The '>>6'
+  //operation divides out the 64 and the '&MASK' is used so that the
+  //argument to the complex exponent is inside our table which has
+  //range 0=>2pi.
+  if(idx<nnum)
+    {
+      i = idx / (num2 * num3);
+      j = (idx / num3) % num2;
+      k = idx % num3;
+      iarg = iarg0 + i * iincu + j * iincv + k * iincw;
+      iadd = iarg >> 6;
+      iadd = iadd & MASK;
+      d_tcsf[idx] = cuCaddf(d_tcsf[idx],d_cex[iadd]);
+    };
+  __syncthreads();
+}
+
+__global__ void four_strucf_cuda_kernel_2d(cuFloatComplex* d_cex, cuFloatComplex* d_tcsf,
+					   int num1, int num2, int num3, int nnum,
+					   int iarg0, int iincu, int iincv, int iincw)
 {
   int i, j, iadd, iarg;
   unsigned int idx = blockDim.x*blockIdx.x + threadIdx.x;
@@ -69,8 +99,8 @@ __global__ void four_strucf_cuda_kernel(cuFloatComplex* d_cex, cuFloatComplex* d
   //range 0=>2pi.
   if(idx<nnum)
     {
-      i = idx / num1;
-      j = idx % num1;
+      i = idx / num2;
+      j = idx % num2;
       iarg = iarg0 + i * iincu + j * iincv;
       iadd = iarg >> 6;
       iadd = iadd & MASK;
