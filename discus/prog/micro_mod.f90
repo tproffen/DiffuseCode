eb5100e840bb963dc1f0cd72338d9eeb51c5e57d
Ross Whitfield <whitfieldre@ornl.gov>
2014-02-27 13:01:37 -0500

    Some small changes to DISCUS to allow compiling with PGI Fortran.

--- discus/prog/micro_mod.f90
+++ discus/prog/micro_mod.f90
@@ -30,12 +30,12 @@ INTEGER, PARAMETER  ::  mk_SYM_ADD_MAX  =  14
 INTEGER             ::  mk_gen_add_n                  = 0
 INTEGER             ::  mk_gen_add_power(mk_GEN_ADD_MAX) = 1
 REAL                ::  mk_gen_add(4,4,0:mk_GEN_ADD_MAX) = &
-         RESHAPE((/(1.,(0.,0.,0.,0.,1.,ik=1,3),il=0,mk_GEN_ADD_MAX)/),SHAPE(mk_gen_add ))
+     RESHAPE((/(1.,(0.,0.,0.,0.,1.,ik=1,3),il=0,mk_GEN_ADD_MAX)/),(/4,4,mk_GEN_ADD_MAX+1/))
 !
 INTEGER             ::  mk_sym_add_n                  = 0
 INTEGER             ::  mk_sym_add_power(mk_SYM_ADD_MAX) = 1
 REAL                ::  mk_sym_add(4,4,0:mk_SYM_ADD_MAX) = &
-         RESHAPE((/(1.,(0.,0.,0.,0.,1.,ik=1,3),il=0,mk_SYM_ADD_MAX)/),SHAPE(mk_sym_add ))
+     RESHAPE((/(1.,(0.,0.,0.,0.,1.,ik=1,3),il=0,mk_SYM_ADD_MAX)/),(/4,4,mk_SYM_ADD_MAX+1/))
 !
 LOGICAL             :: mk_infile_internal  ! File with domain atoms in stored internally
 INTEGER             :: mk_iatom            ! Current atom number in internal file
