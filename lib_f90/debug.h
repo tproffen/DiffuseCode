ed60be6c1a7e87342cab6101d46a37ddc3b5bebb
Justin M Wozniak <wozniak@mcs.anl.gov>
2014-03-13 15:43:20 -0500

    More debugging macros

--- lib_f90/debug.h
+++ lib_f90/debug.h
@@ -5,9 +5,9 @@
 */
 
 #define TRACE_MACROS 1
-#define DEBUG 1
+#define DEBUG 0
 
-#if TRACE_MACROS==1
+#if TRACE_MACROS==0
 
 #define MACRO(token) print *, "MACRO: ", token
 
@@ -21,10 +21,14 @@
 
 /** Show a message */
 #define MSG(message)  print *, "F90: ", message
+/** Show a message with file info */
 #define MSGF(message)  print *, "F90: ", __FILE__, __LINE__, message
 
 /** Show a variable */
-#define VAR(variable) print *, "F90: ", __FILE__, __LINE__,     \
+#define VAR(variable) print *, "F90: ", __FILE__, __LINE__, \
+    "variable: ", variable
+/** Show a variable with file info */
+#define VARF(variable) print *, "F90: ", __FILE__, __LINE__,     \
     "variable: ", variable
 /** Show a vector */
 #define VEC(variable) print *, "F90: ", "variable:", achar(10), \
