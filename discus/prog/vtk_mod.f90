51d71895629738705db6842b396ee511ca94b453
Ross Whitfield <whitfieldre@ornl.gov>
2014-05-08 15:08:55 -0400

    vtk_mod now using libf90 file open. Also fixed some formats.

--- discus/prog/vtk_mod.f90
+++ discus/prog/vtk_mod.f90
@@ -1,11 +1,11 @@
 module vtk_mod
-  
   contains
   
   subroutine vtk_write (value, laver)
     use diffuse_mod
     use crystal_mod
     use output_mod
+    use errlist_mod
     implicit none
     integer, intent(in) :: value
     logical, intent(in) :: laver
@@ -15,21 +15,22 @@ module vtk_mod
     dy=sqrt(out_vi(1,2)**2+out_vi(2,2)**2+out_vi(3,2)**2)
     dz=sqrt(out_vi(1,3)**2+out_vi(2,3)**2+out_vi(3,3)**2)
     
-    open(10,file=outfile,form='FORMATTED',&
-         access='SEQUENTIAL',action='WRITE',status='REPLACE')
-    write(10,'(A)')'# vtk DataFile Version 3.0'
-    write(10,'(A)')'DISCUS OUTPUT'
-    write(10,'(A)')'ASCII'
-    write(10,'(A)')'DATASET STRUCTURED_POINTS'
-    write(10,'(A,3(X,I0))')'DIMENSIONS',out_inc(1),out_inc(2),out_inc(3)
-    write(10,'(A,3(X,F0.4))')'ORIGIN',out_eck(1,1),out_eck(2,1),out_eck(3,1)
-    write(10,'(A,3(X,F0.4))')'SPACING',dx,dy,dz
-    write(10,'(A,X,I0)'),'POINT_DATA',out_inc(1)*out_inc(2)*out_inc(3)
-    write(10,'(A)')'SCALARS values float'
-    write(10,'(A)')'LOOKUP_TABLE default'
-    write(10,'(E13.6E2)') dsi
+    call oeffne (2, outfile, 'unknown')
+    if (ier_num.ne.0) return
     
-    close(10)
+    write(2,'(A)') '# vtk DataFile Version 3.0'
+    write(2,'(A)') 'DISCUS OUTPUT'
+    write(2,'(A)') 'ASCII'
+    write(2,'(A)') 'DATASET STRUCTURED_POINTS'
+    write(2,'(A,3(1X,I0))') 'DIMENSIONS',out_inc(1),out_inc(2),out_inc(3)
+    write(2,'(A,3(1X,F0.4))') 'ORIGIN',out_eck(1,1),out_eck(2,1),out_eck(3,1)
+    write(2,'(A,3(1X,F0.4))') 'SPACING',dx,dy,dz
+    write(2,'(A,1X,I0)') 'POINT_DATA',out_inc(1)*out_inc(2)*out_inc(3)
+    write(2,'(A)') 'SCALARS values float'
+    write(2,'(A)') 'LOOKUP_TABLE default'
+    write(2,'(E13.6E2)') dsi
+    
+    close(2)
     
   end subroutine vtk_write
 
