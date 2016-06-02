1d0d7ce781a1b4dee7629f9eab0872303b643c9d
rneder <reinhard.neder@fau.de>
2016-06-02 16:40:50 +0200

    Added dummy file related to unified 'run_mpi' command

---
+++ lib_f90/dummy_loop_mpi.f90
@@ -0,0 +1,14 @@
+SUBROUTINE dummy_loop_mpi(prog_n, prog_l, mac_n, mac_l, out_n, out_l, repeat, nindiv)
+!
+! Dummy function for formal reasons shall never be called
+!
+CHARACTER (LEN=*), INTENT(IN) :: prog_n
+CHARACTER (LEN=*), INTENT(IN) :: mac_n
+CHARACTER (LEN=*), INTENT(IN) :: out_n
+INTEGER          , INTENT(IN) :: prog_l
+INTEGER          , INTENT(IN) :: mac_l
+INTEGER          , INTENT(IN) :: out_l
+LOGICAL          , INTENT(IN) :: repeat
+INTEGER          , INTENT(IN) :: nindiv
+!
+END SUBROUTINE dummy_loop_mpi
