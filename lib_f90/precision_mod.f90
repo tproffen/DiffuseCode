fa0ad32872d920b1a47911390fc54e4bd91b04cc
rneder <reinhard.neder@fau.de>
2016-06-06 15:27:28 +0200

    Added KIND for large integer

--- lib_f90/precision_mod.f90
+++ lib_f90/precision_mod.f90
@@ -1,4 +1,6 @@
 MODULE precision_mod
 !
+INTEGER, PARAMETER:: PREC_INT_LARGE=MAX(SELECTED_INT_KIND(8) , &
+                                        SELECTED_INT_KIND(16) ) 
 INTEGER, PARAMETER:: PREC_DP=KIND(0.D0)  ! double precision
 END MODULE precision_mod
