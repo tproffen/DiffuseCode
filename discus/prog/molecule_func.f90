ceb2994f4ddabde616159165c8bb0bf59931dcd4
Reinhard <reinhard.neder@fau.de>
2017-03-31 15:23:46 +0200

    Extended molecularize command to work on previous molecules

--- discus/prog/molecule_func.f90
+++ discus/prog/molecule_func.f90
@@ -117,6 +117,7 @@ INTEGER   :: jatom
 INTEGER   :: katom
 INTEGER   :: n_new,n_atom, n_type, n_mole
 INTEGER   :: i, j
+INTEGER   :: imole
 !
 jatom   = iatoms(1)                        ! Set central atom number
 IF(.NOT. btest(cr_prop(jatom),1)) THEN      ! Atom is not yet inside a molecule
@@ -172,7 +173,41 @@ IF(.NOT. btest(cr_prop(jatom),1)) THEN      ! Atom is not yet inside a molecule
       ENDIF
    ENDDO
 !
-!write(*,*) ' STATUS ', t_list
+   DEALLOCATE(t_list)
+!
+ELSE                                          ! Atom is already inside a molecule
+!
+   ALLOCATE(t_list(1:cr_natoms))              ! Make temporary list of atoms already in the molecule
+   t_list(:)     = .FALSE.
+   t_list(jatom) = .TRUE.
+!
+!  Accumulate all neighbors of the current atom into temp_list
+!
+   IF(natom==1) THEN
+      CALL mol_add_conn(jatom)
+   ELSE
+      CALL mol_add_excl(jatom, natom, iatoms)
+   ENDIF
+   imole = cr_mole(jatom)                    ! Atom is in this molecule
+   n_new = 0
+   DO i=1, UBOUND(t_list,1)
+      IF(t_list(i) .AND. cr_mole(i)/=imole) THEN
+         n_new = n_new + 1
+      ENDIF
+   ENDDO
+!
+   CALL molecule_shift(n_new, imole+1)
+   j = mole_len(imole)
+   DO i=1,UBOUND(t_list,1)
+      IF(t_list(i).AND.cr_mole(i)/=imole) THEN
+         j = j + 1
+         mole_cont(mole_off(imole)+j) = i
+         cr_prop(i) = ibset(cr_prop(i),PROP_MOLECULE)
+         cr_mole(i) = imole
+      ENDIF
+   ENDDO
+   mole_len(imole) = mole_len(imole) + n_new
+!
    DEALLOCATE(t_list)
 !
 ENDIF
@@ -399,5 +434,50 @@ n_new = ifinish - istart + 1            ! no of atoms in new molecule
    ENDDO
 !
 END SUBROUTINE molecularize_numbers
+!
+SUBROUTINE molecule_shift(nshift, istart)
+!
+!   Shifts the atoms in molecule istart and up by nshift atoms further up
+!
+USE discus_allocate_appl_mod
+USE molecule_mod
+USE errlist_mod
+IMPLICIT NONE
+!
+INTEGER, INTENT(IN) :: nshift
+INTEGER, INTENT(IN) :: istart
+!
+INTEGER :: i,j,k
+INTEGER :: n_mole
+INTEGER :: n_type
+INTEGER :: n_atom
+!
+n_atom = MAX(mole_off(mole_num_mole)+mole_len(mole_num_mole)+ nshift, MOLE_MAX_ATOM)
+IF(n_atom > MOLE_MAX_ATOM) THEN
+   n_mole = MOLE_MAX_MOLE
+   n_type = MOLE_MAX_TYPE
+   n_atom = INT(MAX(n_atom, MOLE_MAX_ATOM)*1.25)  ! reserve space for two more molecules+25%
+   CALL alloc_molecule(1, 1, n_mole, n_type, n_atom)
+   IF ( ier_num /= 0) THEN
+      RETURN
+   ENDIF
+ENDIF
+!
+DO i=mole_num_mole, istart, -1
+   DO j=mole_len(i), 1, -1
+      k = mole_off(i) + j
+      mole_cont(k+nshift) = mole_cont(k)
+   ENDDO
+   mole_off(i) = mole_off(i) + nshift
+ENDDO
+IF(istart <= mole_num_mole) THEN
+   DO j=1,mole_len(istart)
+      mole_cont(mole_off(istart-1)+mole_len(istart-1) + j) = 0
+   ENDDO
+ENDIF
+mole_num_atom = mole_num_atom + nshift
+!
+END SUBROUTINE molecule_shift
+!
 !
 END MODULE molecule_func_mod
