d8b7825abb568930a1d6cbb9fd15f9ab0c00a24b
rneder <reinhard.neder@fau.de>
2019-05-16 17:01:24 +0200

    Preparation for MAJOR Update

---
+++ refine/prog/refine_mac_mod.f90
@@ -0,0 +1,5 @@
+MODULE refine_mac_mod
+CHARACTER(LEN=1024)                 :: refine_mac      ! Refinement user macro name
+INTEGER                             :: refine_mac_l    ! Length of character string for macro
+!
+END MODULE refine_mac_mod
