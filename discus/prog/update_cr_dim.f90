a71b1e830b7f8fd3f80f640db595cd8fb501a6ff
rneder <reinhard.neder@fau.de>
2019-06-05 07:54:42 +0200

    Source code cosmetics

--- discus/prog/update_cr_dim.f90
+++ discus/prog/update_cr_dim.f90
@@ -1,41 +1,45 @@
 MODULE update_cr_dim_mod
 !
 CONTAINS
+!
 !********************************************************************** 
-      SUBROUTINE update_cr_dim 
+!
+SUBROUTINE update_cr_dim 
 !-                                                                      
 !     Updates the crystal dimensions to the current values              
 !+                                                                      
-      USE discus_config_mod 
-      USE crystal_mod 
-      IMPLICIT none 
-!                                                                       
-       
-!                                                                       
-      INTEGER i, j 
-!                                                                       
-!     Set initial values                                                
-!                                                                       
-      IF (cr_natoms.gt.0) then 
-         DO j = 1, 3 
-         cr_dim (j, 1) = cr_pos (j, 1) 
-         cr_dim (j, 2) = cr_pos (j, 1) 
-         ENDDO 
-      ELSE 
-         DO j = 1, 3 
-         cr_dim (j, 1) = 1.e10 
-         cr_dim (j, 1) = - 1.e10 
-         ENDDO 
-      ENDIF 
-!                                                                       
-!     Update values from all atoms in crystal                           
-!                                                                       
-      DO i = 1, cr_natoms 
-      DO j = 1, 3 
-      cr_dim (j, 1) = amin1 (cr_dim (j, 1), cr_pos (j, i) ) 
-      cr_dim (j, 2) = amax1 (cr_dim (j, 2), cr_pos (j, i) ) 
-      ENDDO 
-      ENDDO 
-!                                                                       
-      END SUBROUTINE update_cr_dim                  
+USE discus_config_mod 
+USE crystal_mod 
+!
+IMPLICIT none 
+!
+INTEGER :: i, j     ! Counter variables
+!
+!     Set initial values
+!
+IF(cr_natoms > 0) THEN 
+   DO j = 1, 3 
+      cr_dim(j, 1) = cr_pos(j, 1) 
+      cr_dim(j, 2) = cr_pos(j, 1) 
+   ENDDO 
+ELSE 
+   DO j = 1, 3 
+      cr_dim(j, 1) = 1.e10 
+      cr_dim(j, 1) = - 1.e10 
+   ENDDO 
+ENDIF 
+!
+!     Update values from all atoms in crystal
+!
+DO i = 1, cr_natoms 
+   DO j = 1, 3 
+      cr_dim(j, 1) = MIN(cr_dim(j, 1), cr_pos(j, i)) 
+      cr_dim(j, 2) = MIN(cr_dim(j, 2), cr_pos(j, i)) 
+   ENDDO 
+ENDDO 
+!
+END SUBROUTINE update_cr_dim                  
+!
+!********************************************************************** 
+!
 END MODULE update_cr_dim_mod
