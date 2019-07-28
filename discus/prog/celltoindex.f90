4a2d9de473cc99aed64183b76a3eb9e18186d46f
rneder <reinhard.neder@fau.de>
2019-07-28 19:04:02 +0200

    Source code cosmetics

--- discus/prog/celltoindex.f90
+++ discus/prog/celltoindex.f90
@@ -3,48 +3,56 @@ MODULE celltoindex_mod
 CONTAINS
 !
 !*****7*****************************************************************
-      SUBROUTINE celltoindex (icell, isite, iatom) 
+!
+SUBROUTINE celltoindex (icell, isite, iatom) 
 !-                                                                      
 !       calculates in which unit cell on which site the atom <ia> is    
-!+                                                                      
-      USE discus_config_mod 
-      USE crystal_mod 
-      USE errlist_mod 
-      IMPLICIT none 
+!+
+USE discus_config_mod 
+USE crystal_mod 
+USE errlist_mod 
+!
+IMPLICIT none 
+!
+INTEGER, DIMENSION(3), INTENT(IN)  :: icell
+INTEGER              , INTENT(IN)  :: isite
+INTEGER              , INTENT(OUT) :: iatom
 !                                                                       
-       
-!                                                                       
-      INTEGER iatom, isite, icell (3) 
-!                                                                       
-      iatom = ( (icell (3) - 1) * cr_icc (1) * cr_icc (2) +       &
-                (icell (2) - 1) * cr_icc (1) + (icell (1) - 1) )  &
-              * cr_ncatoms + isite        
-!                                                                       
-      END SUBROUTINE celltoindex                    
+iatom = ( (icell(3) - 1) * cr_icc(1) * cr_icc(2) +       &
+          (icell(2) - 1) * cr_icc(1) + (icell(1) - 1) )  &
+        * cr_ncatoms + isite        
+!
+END SUBROUTINE celltoindex                    
+!
 !*****7*****************************************************************
-      SUBROUTINE indextocell (iatom, icell, isite) 
-!-                                                                      
+!
+SUBROUTINE indextocell (iatom, icell, isite) 
+!-
 !       calculates in which unit cell on which site the atom <ia> is    
-!+                                                                      
-      USE discus_config_mod 
-      USE crystal_mod 
-      USE errlist_mod 
-      IMPLICIT none 
-!                                                                       
-       
-!                                                                       
-      INTEGER iatom, isite, icell (3) 
-      INTEGER ia 
-!                                                                       
-      ia = iatom - 1 
-!                                                                       
-      icell (3) = int (ia / cr_icc (1) / cr_icc (2) / cr_ncatoms) + 1
-      ia = ia - (icell (3) - 1) * cr_icc (1) * cr_icc (2) * cr_ncatoms 
-      icell (2) = int (ia / cr_icc (1) / cr_ncatoms) + 1 
-      ia = ia - (icell (2) - 1) * cr_icc (1) * cr_ncatoms 
-      icell (1) = int (ia / cr_ncatoms) + 1 
-      isite = ia - (icell (1) - 1) * cr_ncatoms + 1 
-!                                                                       
-      END SUBROUTINE indextocell                    
+!+
+USE discus_config_mod 
+USE crystal_mod 
+USE errlist_mod 
+!
+IMPLICIT none 
+!
+INTEGER              , INTENT(IN)  :: iatom
+INTEGER, DIMENSION(3), INTENT(OUT) :: icell
+INTEGER              , INTENT(OUT) :: isite
+!
+INTEGER :: ia 
+!
+ia = iatom - 1 
+!
+icell(3) = INT(ia / cr_icc(1) / cr_icc(2) / cr_ncatoms) + 1
+ia       = ia - (icell(3) - 1) * cr_icc (1) * cr_icc (2) * cr_ncatoms 
+icell(2) = INT(ia / cr_icc(1) / cr_ncatoms) + 1 
+ia       = ia - (icell(2) - 1) * cr_icc(1) * cr_ncatoms 
+icell(1) = INT(ia / cr_ncatoms) + 1 
+isite = ia - (icell(1) - 1) * cr_ncatoms + 1 
+!
+END SUBROUTINE indextocell                    
+!
 !*****7*****************************************************************
+!
 END MODULE celltoindex_mod
--- discus/prog/check_blen_mod.f90
+++ discus/prog/check_blen_mod.f90
@@ -4,7 +4,7 @@ CONTAINS
 !
 !*****7*****************************************************************
 !
-      SUBROUTINE check_blen (x, iatom, rmin, rmax, offset) 
+SUBROUTINE check_blen(x, iatom, rmin, rmax, offset) 
 !+                                                                      
 !     checks if atom 'iatom' is within rmin -> rmax away from           
 !     position x(3). All matching atoms are stored in the               
@@ -12,64 +12,67 @@ CONTAINS
 !     atoms are stored in 'atom_pos' to retain information              
 !     about possible periodic boundaries.                               
 !-                                                                      
-      USE discus_config_mod 
-      USE crystal_mod 
-      USE atom_env_mod 
-      USE metric_mod
-      USE param_mod 
-      USE errlist_mod 
-      USE lib_f90_allocate_mod 
-      IMPLICIT none 
+USE discus_config_mod 
+USE crystal_mod 
+USE atom_env_mod 
+USE metric_mod
+USE param_mod 
+USE errlist_mod 
+USE lib_f90_allocate_mod 
 !                                                                       
-       
+IMPLICIT none 
 !                                                                       
-      REAL x (3), offset (3), rmin, rmax 
-      INTEGER iatom 
-      INTEGER  :: n_res
+REAL, DIMENSION(3), INTENT(IN) :: x
+INTEGER           , INTENT(IN) :: iatom 
+REAL              , INTENT(IN) :: rmin
+REAL              , INTENT(IN) :: rmax
+REAL, DIMENSION(3), INTENT(IN) :: offset
+!
+INTEGER  :: n_res
 !                                                                       
-      REAL v (3), dist  !, do_blen 
-      INTEGER j 
-      LOGICAL lspace 
+REAL, DIMENSION(3) :: v
+REAL               :: dist
+INTEGER            :: j 
+LOGICAL, PARAMETER :: lspace = .TRUE.
 !                                                                       
-      lspace = .true. 
-!                                                                       
-      DO j = 1, 3 
-         atom_pos (j, 0 ) = x (j) 
-      ENDDO 
-      DO j = 1, 3 
-      v (j) = cr_pos (j, iatom) + offset (j) 
-      ENDDO 
-      dist = do_blen (lspace, x, v) 
-      IF (dist.ge.rmin.and.dist.le.rmax) then 
-         IF (atom_env (0) .lt.MAX_ATOM_ENV) then 
-            IF(atom_env(0) > MAXPAR_RES) THEN
-               n_res = MAX(atom_env(0), NINT(MAXPAR_RES*1.1+10),CHEM_MAX_NEIG)
-               CALL alloc_param(n_res)
-               MAXPAR_RES = n_res
-            ENDIF
-            IF (atom_env (0) .lt.MAXPAR_RES) then 
-               atom_env (0) = atom_env (0) + 1 
-               atom_env (atom_env (0) ) = iatom 
-               res_para (atom_env (0) ) = dist 
-               res_para (0) = REAL(nint (res_para (0) ) + 1) 
-               DO j = 1, 3 
-                  atom_pos (j, atom_env (0) ) = v (j) 
-               ENDDO 
-               atom_dis (   atom_env (0) ) = dist
-            ELSE 
-               ier_num = - 79 
-               ier_typ = ER_APPL 
-            ENDIF 
-         ELSE 
-            ier_num = - 45 
-            ier_typ = ER_APPL 
-         ENDIF 
+DO j = 1, 3 
+   atom_pos (j, 0 ) = x (j) 
+ENDDO 
+DO j = 1, 3 
+   v (j) = cr_pos (j, iatom) + offset (j) 
+ENDDO 
+dist = do_blen (lspace, x, v) 
+IF (dist >= rmin.AND.dist <= rmax) THEN 
+   IF (atom_env(0) < MAX_ATOM_ENV) THEN 
+      IF(atom_env(0) > MAXPAR_RES) THEN
+         n_res = MAX(atom_env(0), NINT(MAXPAR_RES*1.1+10),CHEM_MAX_NEIG)
+         CALL alloc_param(n_res)
+         MAXPAR_RES = n_res
+      ENDIF
+      IF (atom_env(0) < MAXPAR_RES) THEN 
+         atom_env(0) = atom_env(0) + 1 
+         atom_env(atom_env(0) ) = iatom 
+         res_para(atom_env(0) ) = dist 
+         res_para(0) = REAL(NINT(res_para(0) ) + 1) 
+         DO j = 1, 3 
+            atom_pos(j, atom_env(0) ) = v(j) 
+         ENDDO 
+         atom_dis(atom_env(0) ) = dist
+      ELSE 
+         ier_num = - 79 
+         ier_typ = ER_APPL 
       ENDIF 
-      END SUBROUTINE check_blen                     
+   ELSE 
+      ier_num = - 45 
+      ier_typ = ER_APPL 
+   ENDIF 
+ENDIF 
+!
+END SUBROUTINE check_blen                     
 !
 !*****7*****************************************************************
 !
-      SUBROUTINE check_blen_mol (x, imole, rmin, rmax, offset) 
+SUBROUTINE check_blen_mol(x, imole, rmin, rmax, offset) 
 !+                                                                      
 !     checks if molecule 'imole' is within rmin -> rmax awaz from       
 !     position x(3). All matching molecules are stored in the           
@@ -77,44 +80,50 @@ CONTAINS
 !     molecules are stored in 'mole_pos' to retain information          
 !     about possible periodic boundaries.                               
 !-                                                                      
-      USE discus_config_mod 
-      USE crystal_mod 
-      USE metric_mod
-      USE molecule_mod 
-      USE mole_env_mod 
-       
-      USE errlist_mod 
-      USE param_mod 
-      IMPLICIT none 
+USE discus_config_mod 
+USE crystal_mod 
+USE metric_mod
+USE molecule_mod 
+USE mole_env_mod 
+!
+USE errlist_mod 
+USE param_mod 
+!                                                                       
+IMPLICIT none 
+!                                                                       
+REAL, DIMENSION(3), INTENT(IN) :: x
+INTEGER           , INTENT(IN) :: imole 
+REAL              , INTENT(IN) :: rmin
+REAL              , INTENT(IN) :: rmax 
+REAL, DIMENSION(3), INTENT(IN) :: offset
+!                                                                       
+REAL v (3), dist !, do_blen 
+INTEGER i, j 
+LOGICAL, PARAMETER :: lspace =.TRUE.
 !                                                                       
 !                                                                       
-      REAL x (3), offset (3), rmin, rmax 
-      INTEGER imole 
-!                                                                       
-      REAL v (3), dist !, do_blen 
-      INTEGER i, j 
-      LOGICAL lspace 
-!                                                                       
-      lspace = .true. 
-!                                                                       
-      i = mole_cont (mole_off (imole) + 1) 
+i = mole_cont(mole_off(imole) + 1) 
+DO j = 1, 3 
+   v(j) = cr_pos(j, i) + offset(j) 
+ENDDO 
+dist = do_blen(lspace, x, v) 
+IF (dist >= rmin.AND.dist <= rmax) THEN 
+   IF (mole_env(0) <   MAX_MOLE_ENV) THEN 
+      mole_env(0) = mole_env (0) + 1 
+      mole_env(mole_env(0) ) = imole 
+      res_para(mole_env(0) ) = dist 
+      res_para(0) = REAL(NINT(res_para(0) ) + 1) 
       DO j = 1, 3 
-      v (j) = cr_pos (j, i) + offset (j) 
+         mole_pos (j, mole_env(0) ) = v (j) 
       ENDDO 
-      dist = do_blen (lspace, x, v) 
-      IF (dist.ge.rmin.and.dist.le.rmax) then 
-         IF (mole_env (0) .lt.MAX_MOLE_ENV) then 
-            mole_env (0) = mole_env (0) + 1 
-            mole_env (mole_env (0) ) = imole 
-            res_para (mole_env (0) ) = dist 
-            res_para (0) = REAL(nint (res_para (0) ) + 1) 
-            DO j = 1, 3 
-            mole_pos (j, mole_env (0) ) = v (j) 
-            ENDDO 
-         ELSE 
-            ier_num = - 45 
-            ier_typ = ER_APPL 
-         ENDIF 
-      ENDIF 
-      END SUBROUTINE check_blen_mol                 
+   ELSE 
+      ier_num = - 45 
+      ier_typ = ER_APPL 
+   ENDIF 
+ENDIF 
+!
+END SUBROUTINE check_blen_mol                 
+!
+!*******************************************************************************
+!
 END MODULE check_blen_mod
