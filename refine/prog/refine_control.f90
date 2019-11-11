769969c39bf6280aa6c3d92facafe56ea2f6d9dc
rneder <reinhard.neder@fau.de>
2019-11-11 09:40:02 +0100

    Added variable for MRQ initialization

--- refine/prog/refine_control.f90
+++ refine/prog/refine_control.f90
@@ -8,5 +8,8 @@ REAL    :: conv_dchi2    = 0.5   ! Maximum Chi^2 change for convergence
 REAL    :: conv_chi2     = 0.5   ! Minimum Chi^2 value  for convergence
 REAL    :: conv_conf     = 0.01  ! Minimum confidence level
 LOGICAL :: lconvergence  = .FALSE. ! Convergence has been reached
+REAL    :: refine_lamda_s  = 0.001   ! MRQ lamda start
+REAL    :: refine_lamda_u  = 4.000   ! MRQ lamda Increase (up)
+REAL    :: refine_lamda_d  = 0.500   ! MRQ lamda Decrease (down)
 !
 END MODULE refine_control_mod
