4f0bd74a146618c8abc1a6cae2be2a50207eadb9
rneder <reinhard.neder@fau.de>
2019-11-11 09:38:44 +0100

    Source code cosmetics

--- lib_f90/exit_mod.f90
+++ lib_f90/exit_mod.f90
@@ -1,49 +1,64 @@
 MODULE exit_mod
 !
 CONTAINS
+!
 !*****7*****************************************************************
-      SUBROUTINE exit_all 
+!
+SUBROUTINE exit_all 
 !                                                                       
 !     This part contains all the program independent commands.          
 !                                                                       
-      USE errlist_mod 
-      USE prompt_mod 
-      USE operating_mod
-      USE terminal_mod
-      IMPLICIT none 
+USE envir_mod
+USE errlist_mod 
+USE prompt_mod 
+USE operating_mod
+USE terminal_mod
 !                                                                       
+IMPLICIT none 
 !
-      INTEGER socket_send
+CHARACTER(LEN=1024) :: tempfile   ! jmol script files to remove
+CHARACTER(LEN=1024) :: line       ! temporary string
+INTEGER             :: socket_send
+LOGICAL             :: lpresent
 !                                                                       
-      IF (ier_num.ne.0) THEN 
-         CALL errlist 
-      ENDIF 
+IF (ier_num.ne.0) THEN 
+   CALL errlist 
+ENDIF 
+!
+!------ Delete JMOL scripts
+!
+WRITE(tempfile, '(a,i5.5,a)') '/tmp/jmol.',PID,'.00001.mol'
+INQUIRE(FILE=tempfile,EXIST=lpresent)
+IF(lpresent) THEN
+   line = 'rm -f ' // tempfile(1:len_trim(tempfile)-9) // '*.mol'
+   CALL system(line)
+ENDIF
 !                                                                       
 !------ Close output file                                               
 !                                                                       
-      IF (output_status.ne.OUTPUT_SCREEN) THEN 
-         CLOSE (output_io) 
-      ENDIF 
+IF (output_status.ne.OUTPUT_SCREEN) THEN 
+   CLOSE (output_io) 
+ENDIF 
 !                                                                       
 !------ Close sockets                                                   
 !                                                                       
-      IF (lremote) THEN 
-         ier_num =  socket_send (s_remote, 'bye', 3) 
-         CALL socket_close (s_remote) 
-      ENDIF 
+IF (lremote) THEN 
+   ier_num =  socket_send (s_remote, 'bye', 3) 
+   CALL socket_close (s_remote) 
+ENDIF 
 !                                                                       
-      IF (lsocket) THEN 
-         CALL socket_close (s_conid) 
-         CALL socket_close (s_sock) 
-      ENDIF 
+IF (lsocket) THEN 
+   CALL socket_close (s_conid) 
+   CALL socket_close (s_sock) 
+ENDIF 
 !
 !       For library procedure style set setup_done to false
 !
-      lsetup_done = .false.
+lsetup_done = .false.
 !
-      CALL operating_exit   ! Exit for operating system
-      write(*,'(a,a)') COLOR_BG_DEFAULT, COLOR_FG_DEFAULT
+CALL operating_exit   ! Exit for operating system
+write(*,'(a,a)') COLOR_BG_DEFAULT, COLOR_FG_DEFAULT
 !                                                                       
-      END SUBROUTINE exit_all                       
+END SUBROUTINE exit_all                       
 !
 END MODULE exit_mod
