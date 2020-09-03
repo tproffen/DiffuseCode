2567a5e4e2952da20b2fba6a48efd74073567043
rneder <reinhard.neder@fau.de>
2020-09-03 18:01:54 +0200

    Phased out discus_config

--- discus/prog/atom_env_mod.f90
+++ discus/prog/atom_env_mod.f90
@@ -3,13 +3,12 @@ MODULE atom_env_mod
 !
 !     include file for atomic environment variables
 !-
-USE discus_config_mod
-!
 SAVE
 !
+INTEGER, PARAMETER  ::  MAX_ATOM_ENV    =  6000
 INTEGER, PARAMETER  ::  MAX_ATOM_ENV_D  =  MAX_ATOM_ENV+1
 !
-INTEGER             ::  atom_env(0:MAX_ATOM_ENV) = 0
+INTEGER             ::  atom_env(  0:MAX_ATOM_ENV) = 0
 REAL                ::  atom_pos(3,0:MAX_ATOM_ENV) = 0.0
 REAL                ::  atom_dis(  0:MAX_ATOM_ENV) = 0.0
 !
