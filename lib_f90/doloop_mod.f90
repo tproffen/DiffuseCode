9a451c730f1d95cd6eb6e230825faa431464c49e
rneder <reinhard.neder@fau.de>
2020-10-08 22:28:53 +0200

    Adapdted precision of loop counters

--- lib_f90/doloop_mod.f90
+++ lib_f90/doloop_mod.f90
@@ -2,22 +2,24 @@ MODULE doloop_mod
 !+
 !     This file contains Variable definitions for do loop's, if's
 !-
-   IMPLICIT NONE
+USE precision_mod
 !
-   PUBLIC
-   SAVE
+IMPLICIT NONE
 !
-   INTEGER, PARAMETER           :: MAXLEV = 20
-   INTEGER, PARAMETER           :: MAXCOM = 2000
+PUBLIC
+SAVE
 !
-   INTEGER, DIMENSION(0:MAXLEV) :: nloop    = 0       ! (0:MAXLEV)
-   INTEGER, DIMENSION(0:MAXLEV) :: iloop    = 0       ! (0:MAXLEV)
-   INTEGER, DIMENSION(1:1     ) :: do_kpara = 0       ! (1)
+INTEGER, PARAMETER           :: MAXLEV = 20
+INTEGER, PARAMETER           :: MAXCOM = 2000
 !
-   LOGICAL, DIMENSION(0:MAXLEV) :: ldostart = .false. ! (0:MAXLEV)
+INTEGER, DIMENSION(0:MAXLEV) :: nloop    = 0       ! (0:MAXLEV)
+INTEGER, DIMENSION(0:MAXLEV) :: iloop    = 0       ! (0:MAXLEV)
+INTEGER, DIMENSION(1:1     ) :: do_kpara = 0       ! (1)
 !
-   REAL   , DIMENSION(0:MAXLEV) :: ghigh    = 0.0     ! (0:MAXLEV)
-   REAL   , DIMENSION(0:MAXLEV) :: glow     = 0.0     ! (0:MAXLEV)
-   REAL   , DIMENSION(0:MAXLEV) :: ginc     = 1.0     ! (0:MAXLEV)
+LOGICAL, DIMENSION(0:MAXLEV) :: ldostart = .false. ! (0:MAXLEV)
+!
+REAL(KIND=PREC_DP), DIMENSION(0:MAXLEV) :: ghigh    = 0.0     ! (0:MAXLEV)
+REAL(KIND=PREC_DP), DIMENSION(0:MAXLEV) :: glow     = 0.0     ! (0:MAXLEV)
+REAL(KIND=PREC_DP), DIMENSION(0:MAXLEV) :: ginc     = 1.0     ! (0:MAXLEV)
 !
 END MODULE doloop_mod
