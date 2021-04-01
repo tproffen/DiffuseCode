ac5617957d9afbe0936d5855b3e67d98a5cc3b7a
rneder <reinhard.neder@fau.de>
2021-04-01 15:53:42 +0200

    Cosmetics

--- discus/prog/do_find_mod.f90
+++ discus/prog/do_find_mod.f90
@@ -147,104 +147,110 @@ USE str_comp_mod
       ENDIF 
 !                                                                       
       END SUBROUTINE do_find                        
+!
 !*****7*****************************************************************
-      SUBROUTINE do_find_env (ianz, werte, maxw, x, rmin, rmax, fq, fp) 
+!
+SUBROUTINE do_find_env (ianz, werte, maxw, x, rmin, rmax, fq, fp) 
 !                                                                       
 !     This routine finds all atoms around x with a minimal              
 !     distance of rmin and a maximum distance of rmax.                  
 !-                                                                      
-      USE discus_config_mod 
-      USE crystal_mod 
-      USE atom_env_mod 
-      USE check_bound_mod
-      USE check_blen_mod
-      USE celltoindex_mod
-      USE modify_func_mod
-      USE param_mod 
-      USE errlist_mod 
-      USE sorting_mod 
+USE discus_config_mod 
+USE crystal_mod 
+USE atom_env_mod 
+USE check_bound_mod
+USE check_blen_mod
+USE celltoindex_mod
+USE modify_func_mod
+USE param_mod 
+USE errlist_mod 
+USE sorting_mod 
 USE precision_mod
-      IMPLICIT none 
 !                                                                       
-       
+IMPLICIT none 
+
 !                                                                       
-      INTEGER ianz, maxw 
-      REAL(KIND=PREC_DP) :: werte (maxw) 
-      REAL x (3) 
-      REAL rmin, rmax 
-      LOGICAL fq, fp (3) 
+INTEGER                            , intent(in) :: ianz
+INTEGER                            , intent(in) :: maxw 
+REAL(KIND=PREC_DP), dimension(MAXW), intent(out) :: werte ! (maxw) 
+REAL              , dimension(3)   , intent(in)  :: x     ! (3) 
+REAL                               , intent(in)  :: rmin
+REAL                               , intent(in)  :: rmax 
+LOGICAL                            , intent(in)  :: fq
+LOGICAL           , dimension(3)   , intent(in)  :: fp 
 !                                                                       
-      INTEGER i, j, k, ii 
-      INTEGER :: ix, iy, iz
-      INTEGER :: ix1, ix2, iy1,iy2, iz1,iz2
-      INTEGER istart (3), iend (3), iii (3), cell (3), iatom 
-      REAL offset (3), nooffset (3) 
-      LOGICAL ltype 
-      INTEGER, DIMENSION(  :), ALLOCATABLE :: tmp_ind
-      INTEGER, DIMENSION(  :), ALLOCATABLE :: tmp_env
-      REAL   , DIMENSION(:,:), ALLOCATABLE :: tmp_pos
-      REAL   , DIMENSION(  :), ALLOCATABLE :: tmp_dis
+INTEGER :: i, j, k, ii 
+INTEGER :: ix, iy, iz
+INTEGER :: ix1, ix2, iy1,iy2, iz1,iz2
+INTEGER :: istart (3), iend (3), iii (3), cell (3), iatom 
+REAL    :: offset (3), nooffset (3) 
+LOGICAL :: ltype 
+INTEGER, DIMENSION(  :), ALLOCATABLE :: tmp_ind
+INTEGER, DIMENSION(  :), ALLOCATABLE :: tmp_env
+REAL   , DIMENSION(:,:), ALLOCATABLE :: tmp_pos
+REAL   , DIMENSION(  :), ALLOCATABLE :: tmp_dis
 !                                                                       
-      DATA nooffset / 0.0, 0.0, 0.0 / 
+DATA nooffset / 0.0, 0.0, 0.0 / 
 !                                                                       
-      atom_env (0) = 0 
-      res_para (0) = 0 
-      IF (fq) then 
+atom_env (0) = 0 
+res_para (0) = 0 
+!
+IF(fq) then 
 !                                                                       
 !------ --quick version only looks at neighbouring unit cells           
 !                                                                       
-         DO i = 1, 3 
-         iii (i) = int (x (i) - cr_dim0 (i, 1) ) + 1 
-         istart (i) = iii (i) - 1 - int (rmax / cr_a0 (i) )  -1
-         iend (i) = iii (i) + 1 + int (rmax / cr_a0 (i) )  + 1
-         ENDDO 
+   DO i = 1, 3 
+!     iii(i)    = int(x(i) - cr_dim0(i, 1) ) + 1 
+      iii(i)    = int(x(i) - cr_dim(i, 1) ) + 1 
+      istart(i) = iii(i) - 1 - int(rmax / cr_a0(i) )  -1
+      iend(i)   = iii(i) + 1 + int(rmax / cr_a0(i) )  + 1
+   ENDDO 
 !                                                                       
-         DO k = istart (3), iend (3) 
-         DO j = istart (2), iend (2) 
+   DO k = istart (3), iend (3) 
+      DO j = istart (2), iend (2) 
          DO i = istart (1), iend (1) 
-         cell (1) = i 
-         cell (2) = j 
-         cell (3) = k 
+            cell (1) = i 
+            cell (2) = j 
+            cell (3) = k 
 !                                                                       
-         CALL check_bound (cell, offset, fp, ltype) 
-         IF (ltype) then 
-            DO ii = 1, cr_ncatoms 
-            CALL celltoindex (cell, ii, iatom) 
-              ltype = atom_allowed (iatom, werte, ianz, maxw) .and. &
-                      check_select_status(iatom, .true., cr_prop (iatom),  cr_sel_prop)                    
-            IF (ltype) then 
-               CALL check_blen (x, iatom, rmin, rmax, offset) 
-               IF (ier_num.ne.0) return 
+            CALL check_bound (cell, offset, fp, ltype) 
+            IF(ltype) then 
+               DO ii = 1, cr_ncatoms 
+                  CALL celltoindex (cell, ii, iatom) 
+                    ltype = atom_allowed (iatom, werte, ianz, maxw) .and. &
+                            check_select_status(iatom, .true., cr_prop (iatom),  cr_sel_prop)                    
+                  IF (ltype) then 
+                     CALL check_blen (x, iatom, rmin, rmax, offset) 
+                     IF (ier_num.ne.0) return 
+                  ENDIF 
+               ENDDO 
             ENDIF 
-            ENDDO 
-         ENDIF 
 !                                                                       
          ENDDO 
-         ENDDO 
-         ENDDO 
+      ENDDO 
+   ENDDO 
 !                                                                       
 !     --exact: loop over all atoms in crystal that have been added      
 !                                                                       
-         DO i = cr_ncatoms * cr_icc (1) * cr_icc (2) * cr_icc (3)       &
-         + 1, cr_natoms                                                 
+   DO i = cr_ncatoms*cr_icc(1)*cr_icc(2)*cr_icc(3) + 1, cr_natoms
 !                                                                       
-         IF (fp (1) .or.fp (2) .or.fp (3) ) then 
-            ier_num = - 16 
-            ier_typ = ER_CHEM 
-            ier_msg(1) = 'Number of atoms in crystal is larger than'
-            ier_msg(2) = 'atoms_per_unit_cell*number_of_unit_cells'
-            ier_msg(3) = 'Use set crystal, 1, 1, 1,n[1] to correct'
-            RETURN 
-         ENDIF 
+      IF(fp(1) .or. fp(2) .or. fp(3)) then 
+         ier_num = - 16 
+         ier_typ = ER_CHEM 
+         ier_msg(1) = 'Number of atoms in crystal is larger than'
+         ier_msg(2) = 'atoms_per_unit_cell*number_of_unit_cells'
+         ier_msg(3) = 'Use set crystal, 1, 1, 1,n[1] to correct'
+         RETURN 
+      ENDIF 
 !                                                                       
-         ltype = atom_allowed (i, werte, ianz, maxw)                &
-         .and.check_select_status (i, .true., cr_prop (i), cr_sel_prop)    
-         IF (ltype) then 
-            CALL check_blen (x, i, rmin, rmax, nooffset) 
-            IF (ier_num.ne.0) return 
-         ENDIF 
-         ENDDO 
-      ELSE 
+      ltype = atom_allowed(i, werte, ianz, maxw)                &
+      .and.check_select_status (i, .true., cr_prop (i), cr_sel_prop)    
+      IF (ltype) then 
+         CALL check_blen (x, i, rmin, rmax, nooffset) 
+         IF (ier_num /= 0) return 
+      ENDIF 
+   ENDDO 
+ELSE 
 !                                                                       
 !     --exact: loop over all atoms in crystal                           
 !                                                                       
@@ -278,24 +284,24 @@ USE precision_mod
 !           iz1 = 0
 !           iz2 = 0
 !        ENDIF
-         ix1 = 0
-         ix2 = 0
-         iy1 = 0
-         iy2 = 0
-         iz1 = 0
-         iz2 = 0
-         IF(fp(1)) THEN
-            IF(x(1)       -cr_dim(1,1) < rmax*1.5) ix1 = -1
-            IF(cr_dim(1,2)-x(1)        < rmax*1.5) ix2 =  1
-         ENDIF
-         IF(fp(2)) THEN
-            IF(x(2)       -cr_dim(2,1) < rmax*1.5) iy1 = -1
-            IF(cr_dim(2,2)-x(2)        < rmax*1.5) iy2 =  1
-         ENDIF
-         IF(fp(3)) THEN
-            IF(x(3)       -cr_dim(3,1) < rmax*1.5) iz1 = -1
-            IF(cr_dim(3,2)-x(3)        < rmax*1.5) iz2 =  1
-         ENDIF
+   ix1 = 0
+   ix2 = 0
+   iy1 = 0
+   iy2 = 0
+   iz1 = 0
+   iz2 = 0
+   IF(fp(1)) THEN
+      IF(x(1)       -cr_dim(1,1) < rmax*1.5) ix1 = -1
+      IF(cr_dim(1,2)-x(1)        < rmax*1.5) ix2 =  1
+   ENDIF
+   IF(fp(2)) THEN
+      IF(x(2)       -cr_dim(2,1) < rmax*1.5) iy1 = -1
+      IF(cr_dim(2,2)-x(2)        < rmax*1.5) iy2 =  1
+   ENDIF
+   IF(fp(3)) THEN
+      IF(x(3)       -cr_dim(3,1) < rmax*1.5) iz1 = -1
+      IF(cr_dim(3,2)-x(3)        < rmax*1.5) iz2 =  1
+   ENDIF
    DO i = 1, cr_natoms 
       ltype = atom_allowed (i, werte, ianz, maxw)                    &
              .and.check_select_status (i, .true., cr_prop (i), cr_sel_prop)    
@@ -310,7 +316,7 @@ USE precision_mod
                ENDDO
             ENDDO
          ENDDO
-         IF (ier_num /= 0) RETURN
+         IF(ier_num /= 0) RETURN
       ENDIF 
    ENDDO 
 ENDIF 
@@ -318,27 +324,27 @@ ENDIF
 !     Sort neighbors according to distance
 !
 IF(atom_env(0)>0) THEN
-      ALLOCATE(tmp_ind(  1:atom_env(0)))
-      ALLOCATE(tmp_env(  1:atom_env(0)))
-      ALLOCATE(tmp_pos(3,0:atom_env(0)))
-      ALLOCATE(tmp_dis(  1:atom_env(0)))
-      tmp_env    = atom_env(1:atom_env(0))
-      tmp_ind    = 0
-      tmp_pos    = atom_pos
-      tmp_dis    = atom_dis(1:atom_env(0))
-      CALL indexx(atom_env(0),tmp_dis,tmp_ind)
-      DO i=1,atom_env(0)
-         atom_env(i)   = tmp_env(  tmp_ind(i))
-         atom_pos(:,i) = tmp_pos(:,tmp_ind(i))
-         atom_dis(i)   = tmp_dis(  tmp_ind(i))
-      ENDDO
-      DEALLOCATE(tmp_ind)
-      DEALLOCATE(tmp_env)
-      DEALLOCATE(tmp_pos)
-      DEALLOCATE(tmp_dis)
+   ALLOCATE(tmp_ind(  1:atom_env(0)))
+   ALLOCATE(tmp_env(  1:atom_env(0)))
+   ALLOCATE(tmp_pos(3,0:atom_env(0)))
+   ALLOCATE(tmp_dis(  1:atom_env(0)))
+   tmp_env    = atom_env(1:atom_env(0))
+   tmp_ind    = 0
+   tmp_pos    = atom_pos
+   tmp_dis    = atom_dis(1:atom_env(0))
+   CALL indexx(atom_env(0),tmp_dis,tmp_ind)
+   DO i=1,atom_env(0)
+      atom_env(i)   = tmp_env(  tmp_ind(i))
+      atom_pos(:,i) = tmp_pos(:,tmp_ind(i))
+      atom_dis(i)   = tmp_dis(  tmp_ind(i))
+   ENDDO
+   DEALLOCATE(tmp_ind)
+   DEALLOCATE(tmp_env)
+   DEALLOCATE(tmp_pos)
+   DEALLOCATE(tmp_dis)
 ENDIF
 !                                                                       
-      END SUBROUTINE do_find_env                    
+END SUBROUTINE do_find_env                    
 !
 !*****7*****************************************************************
 !
