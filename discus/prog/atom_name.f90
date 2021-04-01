bc3e27d5a5512e32f0d56713358c1b7f76ed4a50
rneder <reinhard.neder@fau.de>
2021-04-01 15:44:11 +0200

    Cosmetics, enhanced handling of atom type '-1'

--- discus/prog/atom_name.f90
+++ discus/prog/atom_name.f90
@@ -1,40 +1,47 @@
 MODULE atom_name
 !
 CONTAINS
+!
 !*****7***************************************************************  
-      CHARACTER(9) function at_name (iscat) 
+!
+CHARACTER(len=9) function at_name (iscat) 
 !+                                                                      
 !     This function builds the atom name as XX(iscat) to be             
 !     able to distinguish between different atom types with             
 !     the same name.                                                    
 !-                                                                      
-      USE discus_config_mod 
-      USE crystal_mod 
+USE discus_config_mod 
+USE crystal_mod 
 USE lib_length
-      IMPLICIT none 
 !                                                                       
-       
+IMPLICIT none 
 !                                                                       
-      CHARACTER(5) istr 
-      INTEGER iscat, il, is 
+CHARACTER(len=5) :: istr 
+INTEGER   :: iscat, il, is 
 !                                                                       
 !                                                                       
-      IF (iscat.ge.100) then 
-         WRITE (istr, 1000) iscat 
-      ELSEIF (iscat.ge.10) then 
-         WRITE (istr, 1100) iscat 
-      ELSE 
-         WRITE (istr, 1200) iscat 
-      ENDIF 
+if(iscat==-1) then
+  at_name ='(*)'
+else
+   IF (iscat.ge.100) then 
+      WRITE (istr, 1000) iscat 
+   ELSEIF (iscat.ge.10) then 
+      WRITE (istr, 1100) iscat 
+   ELSE 
+      WRITE (istr, 1200) iscat 
+   ENDIF 
 !                                                                       
-      il = len_str (cr_at_lis (iscat) ) 
-      is = len_str (istr) 
+   il = len_str (cr_at_lis (iscat) ) 
+   is = len_str (istr) 
 !                                                                       
-      at_name = cr_at_lis (iscat) (1:il) //istr (1:is) 
+   at_name = cr_at_lis (iscat) (1:il) //istr (1:is) 
+endif
 !                                                                       
  1000 FORMAT     ('(',I3,')') 
  1100 FORMAT     ('(',I2,')') 
  1200 FORMAT     ('(',I1,')') 
-      END FUNCTION at_name                          
+END FUNCTION at_name                          
+!
 !*****7***************************************************************  
+!
 END MODULE atom_name
