68e754ada0b755207d2c44c121ca3df7e59d3d27
rneder <reinhard.neder@fau.de>
2021-05-03 09:36:24 +0200

    Added a privete routine for indiviudual extensions

---
+++ discus/prog/private.f90
@@ -0,0 +1,39 @@
+module private_mod
+!-
+!  A module in which private calculations can be performed
+!  The actual file "private.f90" is maintained as this template only. 
+!  Your private modifications may get lost if you keep "private.f90"
+!  within DiffuseCode/discus/prog only.
+!
+!  Compare this module to any of the build in ones to create your own 
+!  stuff.
+!+
+contains
+!
+!*******************************************************************************
+!
+subroutine do_private(line)
+!-
+!  Do a private structure modification.
+!  The listed modules which are used serve as example only.
+!+
+use crystal_mod
+use atom_env_mod
+use chem_mod
+use do_find_mod
+!
+use precision_mod
+use param_mod
+!
+implicit none
+!
+character(len=*), intent(inout) :: line
+!
+! With further use of the modules, you can perform private calculations / 
+! modifications of the crystal.
+!
+end subroutine do_private
+!
+!*******************************************************************************
+!
+end module private_mod
