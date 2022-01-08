67c5191924b6c21300b2dddc89655323ab227e06
rneder <reinhard.neder@fau.de>
2022-01-08 18:38:27 +0100

    Extensive Correction and revision

--- discus/prog/perioditize.f90
+++ discus/prog/perioditize.f90
@@ -15,6 +15,7 @@ integer :: pdt_nsite = 0  ! Number of sites in an averaged unit cell
 integer :: pdt_usite = 0  ! Number of sites in an averaged unit cell, defined by user with 'set site'
 integer :: pdt_asite = 0  ! Achieved sites either from set site or find_aver
 integer           , dimension(:,:,:), allocatable :: pdt_cell     ! Atoms per cell
+logical           , dimension(:    ), allocatable :: pdt_lsite    ! This site has been defined
 integer           , dimension(:,  :), allocatable :: pdt_itype    ! Atom types at each site
 integer           , dimension(3)                  :: pdt_ilow = 0    ! Unit cell dimensions in periodic
 integer           , dimension(3)                  :: pdt_ihig = 0    ! low and high inidces
@@ -36,6 +37,7 @@ subroutine perioditize_menu
 use discus_kdo_common_mod
 !
 use calc_expr_mod
+use chem_mod
 use class_macro_internal
 use doact_mod
 use errlist_mod
@@ -146,7 +148,7 @@ loop_menu: do
 !     ----run symmetry 'rese'                                            
 !                                                                       
    if_rese: IF(str_comp (befehl, 'rese', 2, lbef, 4) ) THEN 
-      CALL perioditize_reset
+      call perioditize_reset
       cycle loop_menu
    endif if_rese
 !-------------------------------------------------------------------------------
@@ -166,6 +168,7 @@ loop_menu: do
 !                                                                       
    if(str_comp (befehl, 'run', 2, lbef, 3) ) then
       call perioditize_run
+      call perioditize_reset
    elseif(str_comp (befehl, 'show', 2, lbef, 4) ) then
       call perioditize_show
    elseif(str_comp (befehl, 'set', 2, lbef, 3) ) then
@@ -199,7 +202,7 @@ character(len=*), intent(inout) :: zeile      ! input line
 integer         , intent(inout) :: lp         ! input length
 !
 integer            :: maxw                        ! Actual parameter number
-integer, parameter :: MIN_PARA =  2               ! Minimum parameter numbers
+integer, parameter :: MIN_PARA =  4               ! Minimum parameter numbers
 character(len=PREC_STRING), dimension(MAX(MIN_PARA,MAXSCAT+1)) :: cpara
 integer                   , dimension(MAX(MIN_PARA,MAXSCAT+1)) :: lpara
 real(kind=PREC_DP)        , dimension(MAX(MIN_PARA,MAXSCAT+1)) :: werte
@@ -220,7 +223,9 @@ if(str_comp (cpara(1), 'ncells', 2, lpara(1), 6) ) then
 elseif(str_comp (cpara(1), 'natoms', 2, lpara(1), 6) ) then
    call perioditize_set_natoms(ianz, cpara, lpara, werte, maxw)
 elseif(str_comp (cpara(1), 'site', 2, lpara(2), 4) ) then
-   call perioditize_set_site  (ianz, cpara, lpara, werte, maxw)
+   call perioditize_set_site  (ianz, cpara, lpara, werte, maxw, .false.)
+elseif(str_comp (cpara(1), 'wyck', 2, lpara(2), 4) ) then
+   call perioditize_set_site  (ianz, cpara, lpara, werte, maxw, .true.)
 endif
 !
 end subroutine perioditize_set
@@ -284,6 +289,7 @@ subroutine perioditize_set_natoms(ianz, cpara, lpara, werte, maxw)
 !  Set the number of atoms per unit cell
 !+
 !
+use allocate_generic
 use ber_params_mod
 use errlist_mod
 use precision_mod
@@ -295,6 +301,10 @@ integer :: ianz          ! Number of parameters
 character(len=PREC_STRING), dimension(MAXW), intent(inout)     :: cpara
 integer                   , dimension(MAXW), intent(inout)     :: lpara
 real(kind=PREC_DP)        , dimension(MAXW), intent(inout)     :: werte
+integer :: osite
+integer :: nsite
+integer :: size_of
+integer :: all_status
 !
 cpara(1) = '0'
 lpara(1) = 1
@@ -311,19 +321,28 @@ else
    pdt_usr_nsite = .TRUE.                  ! User fixed number of sites
 endif
 !write(*,'(a, i6, l2)') ' User set number sites ', pdt_nsite, pdt_usr_nsite
+if(pdt_nsite>=ubound(pdt_pos,2)) then 
+   osite = ubound(pdt_pos, 2)
+   nsite = max(pdt_nsite, pdt_usite, pdt_asite) + 5
+!write(*,*) 'PRE REALLOC ', ubound(pdt_pos, 2), osite, nsite, pdt_usite
+   call alloc_arr(pdt_pos, 1, 3, 1, nsite, all_status, 0.0E0,  size_of)
+   call alloc_arr(pdt_lsite, 1, nsite, all_status, .false., size_of)
+endif
 end subroutine perioditize_set_natoms
 !
 !*******************************************************************************
 !
-subroutine perioditize_set_site(ianz, cpara, lpara, werte, maxw)
+subroutine perioditize_set_site(ianz, cpara, lpara, werte, maxw, lwyckoff)
 !-
 !  Set explicit positions expected in the average unit cell
 !+
 !
 use crystal_mod
+use spcgr_apply, only:symmetry_make_wyckoff
 !
 use allocate_generic
 use ber_params_mod
+use berechne_mod
 use errlist_mod
 use get_iscat_mod
 use get_params_mod
@@ -337,48 +356,61 @@ integer :: ianz          ! Number of parameters
 character(len=PREC_STRING), dimension(MAXW), intent(inout)     :: cpara
 integer                   , dimension(MAXW), intent(inout)     :: lpara
 real(kind=PREC_DP)        , dimension(MAXW), intent(inout)     :: werte
+logical                                    , intent(in)        :: lwyckoff
 !
 !
 integer, parameter :: MAXU= 3                   ! Max params for pos:[}
+integer, parameter :: NNMAX= 192                ! Max Wyckoff positions
 character(len=PREC_STRING) :: line
 character(len=PREC_STRING), dimension(MAXSCAT) :: ccpara
 integer                   , dimension(MAXSCAT) :: llpara
 real(kind=PREC_DP)        , dimension(MAXSCAT) :: wwerte
 real(kind=PREC_DP)        , dimension(MAXU)    :: uwerte
+real(kind=PREC_DP)        , dimension(4   )    :: v_pos    ! Symmetry resulting position
+real(kind=PREC_SP)        , dimension(3,192 )  :: wyck     ! Symmetry resulting position
 integer :: iianz          ! Number of parameters
 integer :: jjanz          ! Number of parameters
 integer :: maxww          ! Number of parameters
+integer :: next           ! next site number
 integer :: length
 integer :: osite
 integer :: nsite
 integer :: ntype 
 integer :: size_of
 integer :: all_status
+integer :: istart
+integer :: ifinish
+logical :: success
+integer :: i, j           ! Dummy loop variable
 !
 !
-integer, parameter :: NOPTIONAL = 2
-integer, parameter :: O_ATOM    = 1
-integer, parameter :: O_POS     = 2
+integer, parameter :: NOPTIONAL = 3
+
+integer, parameter :: O_NUMBER  = 1
+integer, parameter :: O_ATOM    = 2
+integer, parameter :: O_POS     = 3
 character(LEN=   6), dimension(NOPTIONAL) :: oname   !Optional parameter names
 character(LEN=PREC_STRING), dimension(NOPTIONAL) :: opara   !Optional parameter strings returned
 integer            , dimension(NOPTIONAL) :: loname  !Lenght opt. para name
 integer            , dimension(NOPTIONAL) :: lopara  !Lenght opt. para name returned
 logical            , dimension(NOPTIONAL) :: lpresent!opt. para is present
 real(kind=PREC_DP) , dimension(NOPTIONAL) :: owerte   ! Calculated values
-integer, parameter                        :: ncalc = 1 ! Number of values to calculate 
+integer, parameter                        :: ncalc = 0 ! Number of values to calculate 
 !
-data oname  / 'atoms ', 'pos'   /
-data loname /  5,    3          /
-opara  =  (/ 'all    ', '[0,0,0]' /)   ! Always provide fresh default values
-lopara =  (/  3,        76        /)
-owerte =  (/  0.0,      0.00      /)
+data oname  / 'number', 'atoms ', 'pos'   /
+data loname /  6      ,  5      ,  3      /
+opara  =  (/ 'next   ', 'all    ', '[0,0,0]' /)   ! Always provide fresh default values
+lopara =  (/  4       ,  3       , 7         /)
+owerte =  (/  -1.0    ,  0.0     , 0.00      /)
 !
 MAXWW = MAXSCAT
 !
 call get_optional(ianz, MAXW, cpara, lpara, NOPTIONAL,  ncalc, &
                   oname, loname, opara, lopara, lpresent, owerte)
+if(ier_num/=0) return
 cpara(1) = '0'
 lpara(1) = 1
+next = -1
 !
 if(cpara(2) == 'auto') then
    pdt_nsite     = 0                       ! No sites found automatically yet
@@ -399,7 +431,9 @@ else
             length = lopara(O_ATOM)
          endif
          call get_params(line, iianz, ccpara, llpara, maxwW, length)
+         if(ier_num/=0) return
          call get_iscat (iianz, ccpara, llpara, wwerte, MAXWW, .FALSE. )   ! No new atom types
+         if(ier_num/=0) return
       endif
    else
       ier_num = -6
@@ -412,22 +446,92 @@ else
       length = lopara(O_POS)
       jjanz = 3
       call get_optional_multi(MAXU, line, length, uwerte, jjanz)
+      if(ier_num/=0) return
+      if(uwerte(1)<0.0 .or. uwerte(1)>1.0 .or.                                  &
+         uwerte(2)<0.0 .or. uwerte(2)>1.0 .or.                                  &
+         uwerte(3)<0.0 .or. uwerte(3)>1.0       ) then
+         ier_num = -50
+         ier_typ = ER_FORT
+         ier_msg(1) = 'At least on of the coordinates outside [0,1]'
+         return
+      endif
    else
       ier_num = -6
       ier_typ = ER_COMM
       ier_msg(1) = '''pos:'' parameter is missing'
       return
    endif
+   if(lpresent(O_NUMBER)) then               ! number: is present
+      if(opara(O_NUMBER)=='next') then
+         next = -1
+      else
+         line   = '(' // opara(O_NUMBER)(1:lopara(O_NUMBER)) // ')'
+         length = lopara(O_NUMBER) + 2
+         next = nint(berechne(line, length))
+         if(ier_num/=0) return
+         if(next<=0) then
+            ier_num = -50
+            ier_typ = ER_FORT
+            ier_msg(1) = 'number:<value> must be > 1'
+            return
+         endif
+      endif
+   else
+      next = -1
+   endif
 endif
 !
-pdt_usr_atom = .TRUE.             ! User did flag atom positions
-pdt_usite = pdt_usite + 1
+pdt_usr_atom = .TRUE.                ! User did flag atom positions
+!
+if(lwyckoff) then                    ! Generate Wyckoff orbit
+  wyck(:,1) = uwerte(1:3)
+  call symmetry_make_wyckoff(NNMAX, wyck, ifinish)
+  istart = 1
+  next = -1
+else                                 ! Just this site
+  istart = 1
+  ifinish = 1
+  wyck(:,1) = uwerte(1:3)
+endif
+!
+do j=istart, ifinish
+!
+   v_pos(1:3) = wyck(1:3,j)
+!  if(j>1) then                      ! Need to apply symmetry operation
+!     
+!     v_pos = matmul(spc_mat(:,:,j),u_pos)
+!     v_pos(1) = v_pos(1) - REAL(INT(v_pos(1)), PREC_DP)
+!     if(v_pos(1)< 0.0) v_pos(1) = v_pos(1) + 1.0D0
+!     v_pos(2) = v_pos(2) - REAL(INT(v_pos(2)), PREC_DP)
+!     if(v_pos(2)< 0.0) v_pos(2) = v_pos(2) + 1.0D0
+!     v_pos(3) = v_pos(3) - REAL(INT(v_pos(3)), PREC_DP)
+!     if(v_pos(3)< 0.0) v_pos(3) = v_pos(3) + 1.0D0
+!  endif
+!
+   
+if(next == -1) then
+   success = .false.
+   search: do i=1, pdt_usite         ! Search for a free site
+      if(.not.pdt_lsite(i)) then     ! this site has not been defined
+         pdt_usite = i
+         pdt_lsite(pdt_usite) = .true.
+         success = .true.
+         exit search
+      endif
+   enddo search
+   if(.not.success) then             ! No free site found, make next
+      pdt_usite = pdt_usite + 1
+   endif
+else
+   pdt_usite = iabs(next)            ! User defined explicit site number
+endif
 pdt_asite = max(pdt_asite, pdt_usite)
 if(pdt_usite>=ubound(pdt_pos,2)) then 
    osite = ubound(pdt_pos, 2)
    nsite = max(pdt_nsite, pdt_usite, pdt_asite) + 5
 !write(*,*) 'PRE REALLOC ', ubound(pdt_pos, 2), osite, nsite, pdt_usite
    call alloc_arr(pdt_pos, 1, 3, 1, nsite, all_status, 0.0E0,  size_of)
+   call alloc_arr(pdt_lsite, 1, nsite, all_status, .false., size_of)
 endif
 if(pdt_usite>=ubound(pdt_itype,2) .or. iianz>ubound(pdt_itype,1)) then
    nsite = ubound(pdt_pos, 2)
@@ -435,9 +539,11 @@ if(pdt_usite>=ubound(pdt_itype,2) .or. iianz>ubound(pdt_itype,1)) then
    call alloc_arr(pdt_itype, 0, ntype, 1, nsite, all_status, -2, size_of)
 endif
 !
-pdt_pos(:,pdt_usite) = uwerte(1:jjanz)
+pdt_pos(:,pdt_usite) = v_pos(1:3)
 pdt_itype(1:iianz, pdt_usite) = nint(wwerte(1:iianz))
 pdt_itype(0      , pdt_usite) = iianz
+pdt_lsite(pdt_usite) = .true.
+enddo
 !
 !write(*,*) 'SITE: ', pdt_usite, pdt_itype(0:iianz,pdt_usite), pdt_pos(1:jjanz,pdt_usite)
 pdt_nsite = max(pdt_nsite, pdt_usite, pdt_asite)
@@ -461,6 +567,8 @@ integer, dimension(3) :: ncell_dummy
 real(kind=PREC_SP) :: aver
 real(kind=PREC_SP) :: sigma
 !
+aver = 0.0
+sigma = 0.0
 if(.not. (pdt_usr_ncell) ) then            ! User did not define number of atoms
    call estimate_ncells(ncell_dummy)
 endif
@@ -475,9 +583,16 @@ if(.not. (pdt_usr_atom) ) then             ! User did not specify unit cell cont
    if(ier_num /= 0) return
 else                                       ! User specified (partial) unit cell content
    if(pdt_usr_nsite) aver = pdt_nsite      ! User specified number of atoms per cell
-   if(pdt_asite*pdt_ncells/=cr_natoms) then ! Number of sites does not match
+   if(pdt_asite*pdt_ncells <cr_natoms) then ! Number of sites does not match
       call find_average(aver)
       if(ier_num /= 0) return
+   elseif(pdt_nsite*pdt_ncells >2.*cr_natoms) then ! Number of sites does not match
+      ier_num = -179
+      ier_typ = ER_APPL
+      ier_msg(1) = 'Expecting too many atoms '
+      write(ier_msg(2),'(a,i9)') 'Expecting : ', pdt_nsite*pdt_ncells
+      write(ier_msg(3),'(a,i9)') 'In crystal: ', cr_natoms
+      return
    endif
 endif
 !
@@ -559,14 +674,14 @@ integer :: i,j,k, l   ! Dummy counter
 integer :: natoms     ! Total atoms in search window
 integer :: maxn       ! Max number per unit cell
 integer           , dimension(3)                  :: ixyz         ! Atom is in this unit cell
-integer           , dimension(0:100)              :: counter
+integer           , dimension(0:500)              :: counter
 !
 allocate(pdt_cell(pdt_ilow(1):pdt_ihig(1), pdt_ilow(2):pdt_ihig(2),pdt_ilow(3):pdt_ihig(3)))
 pdt_cell = 0
 do i=1, cr_natoms
-   ixyz(1) = int(cr_pos(1,i) + 0.5 - pdt_ilow(1)) + pdt_ilow(1)             ! add 0.5 to avoid loosing atoms at the low edge
-   ixyz(2) = int(cr_pos(2,i) + 0.5 - pdt_ilow(2)) + pdt_ilow(2)
-   ixyz(3) = int(cr_pos(3,i) + 0.5 - pdt_ilow(3)) + pdt_ilow(3)
+   ixyz(1) = int(cr_pos(1,i) + 0.05 - pdt_ilow(1)) + pdt_ilow(1)             ! add 0.5 to avoid loosing atoms at the low edge
+   ixyz(2) = int(cr_pos(2,i) + 0.05 - pdt_ilow(2)) + pdt_ilow(2)
+   ixyz(3) = int(cr_pos(3,i) + 0.05 - pdt_ilow(3)) + pdt_ilow(3)
    if(ixyz(1)>=pdt_ilow(1) .and. ixyz(1)<=pdt_ihig(1)   .and.          &
       ixyz(2)>=pdt_ilow(2) .and. ixyz(2)<=pdt_ihig(2)   .and.          &
       ixyz(3)>=pdt_ilow(3) .and. ixyz(3)<=pdt_ihig(3)         ) then
@@ -590,17 +705,14 @@ do k=pdt_ilow(3), pdt_ihig(3)
 enddo
 aver  = 0.0
 sigma = 0.0
-!write(*,*) counter(0:maxn)
 do i=0,maxn
   aver = aver + real(i*counter(i))
 enddo
-!write(*,*) ' Aver, natoms ', aver, natoms, pdt_ncells
 aver = aver/real(pdt_ncells)
 do i=0,maxn
    sigma = sigma + counter(i)*(aver-i)**2
 enddo
 sigma = sqrt(sigma)/real(pdt_ncells*(pdt_ncells-1))
-!write(*,*) ' Av sig ', aver, sigma, nint(aver)*pdt_ncells, cr_natoms
 !
 deallocate(pdt_cell)
 !
@@ -637,7 +749,7 @@ integer           , dimension(  2*nint(aver)) :: pdt_temp_n    ! Number of atoms
 logical           , dimension(  2*nint(aver)) :: is_valid      ! This site is OK T/F
 !
 pdt_nsite = aver                       ! Initialize number of sites per unit cell
-eps = 0.9                              ! Start with 0.5 Angstroem
+eps = 0.30                             ! Start with 0.5 Angstroem
 if(pdt_asite==0) then                  ! No sites yet
    pdt_asite = 1                              ! No sites yet
    pdt_temp(:, 1) =    (cr_pos(:,1) + 0.0 + 2*nint(abs(pdt_dims(:,1)))) - &
@@ -652,8 +764,12 @@ endif
 !
 loop_atoms: do i=istart, cr_natoms
 !  do j=1,3
-   uvw(:) =  (cr_pos(:,i) + 0.0 + 2*nint(abs(pdt_dims(:,1)))) - &
-         nint(cr_pos(:,i) + 0.0 + 2*nint(abs(pdt_dims(:,1))))
+!  uvw(:) =  (cr_pos(:,i) + 0.0 + 2*nint(abs(pdt_dims(:,1)))) - &
+!        nint(cr_pos(:,i) + 0.0 + 2*nint(abs(pdt_dims(:,1))))
+   uvw(:) = cr_pos(:,i) - real(int(cr_pos(:,i)), PREC_SP)   ! Create fractionl
+   if(uvw(1) < 0.0E0) uvw(1) = uvw(1) + 1.0                 ! coordinates in 
+   if(uvw(2) < 0.0E0) uvw(2) = uvw(2) + 1.0                 ! range [0,1]
+   if(uvw(3) < 0.0E0) uvw(3) = uvw(3) + 1.0
 !  enddo
    dmin = 1e12
    ic   = 0
@@ -684,10 +800,18 @@ do j=1, ubound(pdt_temp_n,1)
    endif
 enddo
 !
+if(pdt_asite<pdt_nsite) then                   ! Too few sites, give up
+   ier_num = -179
+   ier_typ = ER_APPL
+   ier_msg(1) = 'Found too few site '
+   return
+endif
 nred = 0
 reduce: do
    if(pdt_asite-nred == pdt_nsite) then        ! Correct site number
       if((pdt_asite-nred)*pdt_ncells==cr_natoms) then
+         exit reduce                           ! Correct site number
+      elseif(pdt_usr_ncell .and. (pdt_asite-nred)*pdt_ncells<2*cr_natoms) then 
          exit reduce                           ! Correct site number
       else
          ier_num = -179
@@ -730,11 +854,16 @@ do j=1, pdt_asite
   if(is_valid(j)) then
     k = k + 1
     pdt_pos(:,k) = pdt_temp(:,j)
+    do i=1,3
+       if(pdt_pos(i,k)< 0.0) pdt_pos(i,k) = pdt_pos(i,k) + 1.0
+       if(pdt_pos(i,k)>=1.0) pdt_pos(i,k) = pdt_pos(i,k) - 1.0
+    enddo
+!write(*,*) ' POS ', pdt_pos(:,k)
   endif
 enddo
 pdt_asite = pdt_asite - nred
 !
-
+!write(*,*) ' PDT_ASITE ', pdt_asite
 end subroutine find_average
 !
 !*******************************************************************************
@@ -756,7 +885,8 @@ implicit none
 !
 logical, parameter :: lspace =.TRUE.
 real(kind=PREC_SP) :: EPS = 2.5
-integer :: i,j,k,l,m,n           ! dummy counters
+logical :: lperiod               ! Test with periodic boundary conditions
+integer :: i,j,k,l,m,n, nn       ! dummy counters
 integer :: nprior                ! Number of atoms in crystal
 integer :: natoms                ! Number of atoms in new crystal
 integer :: ifail                 ! Atom type in case of failure
@@ -783,6 +913,11 @@ else
 !write(*,*) ' pdt_usr_nsite is ', pdt_usr_nsite, pdt_nsite, pdt_ncells
 endif
 !write(*,*) ' NATOMS ', natoms
+!do l=1, pdt_nsite
+!write(*,*) ' SITE   ', pdt_pos(:,l)
+!enddo
+!write(*,*) ' low  ', pdt_ilow
+!write(*,*) ' high ', pdt_ihig
 !
 allocate(tmp_iscat(     1:natoms))
 allocate(tmp_prop (     1:natoms))
@@ -808,6 +943,9 @@ do k=pdt_ilow(3), pdt_ihig(3)
       enddo
    enddo
 enddo
+!write(*,*) 'MIN/max x ', minval(tmp_pos(1,:)), maxval(tmp_pos(1,:))
+!write(*,*) 'MIN/max x ', minval(tmp_pos(2,:)), maxval(tmp_pos(2,:))
+!write(*,*) 'MIN/max x ', minval(tmp_pos(3,:)), maxval(tmp_pos(3,:))
 !do m=1,pdt_nsite
 !write(*,*) ' LL ', tmp_pos(:,m)
 !enddo
@@ -830,9 +968,27 @@ soften: do l=1,2
          if( cr_iscat(m) <0) cycle loop_old          ! Already transfered
          u = cr_pos(:, m)
          dist = do_blen(lspace, u, v)                ! Calculate distance
+!if(            m==1713) then !abs(cr_pos(1,m)+9.2)<0.1 .and. abs(cr_pos(2,m)+10.0)<0.1) then
+!  write(*,*) ' ATOM ', cr_pos(:,m), cr_iscat(m), dist, dmin
+!  write(*,*) ' ATOM ', cr_pos(:,m)+20, cr_iscat(m), dist, dmin
+!endif
          if(dist < dmin) then                 ! Found new shortest distance
             dmin = dist
             k    = m                          ! Archive site
+         endif
+         lperiod = .false.                    ! Test if periodic boundary conditions may be needed
+         do nn = 1, 3
+            if(u(nn)<pdt_ilow(nn)) then
+               u(nn) = u(nn) + pdt_ihig(nn) - pdt_ilow(nn) + 1
+               lperiod = .true.
+            endif
+         enddo 
+         if(lperiod) then    ! Test with periodic boundary conditions
+            dist = do_blen(lspace, u, v)                ! Calculate distance
+            if(dist < dmin) then                 ! Found new shortest distance
+               dmin = dist
+               k    = m                          ! Archive site
+            endif
          endif
       enddo loop_old
       if(dmin < EPS) then                     ! Found close atom
@@ -896,6 +1052,18 @@ if(n /= nprior) then
       endif
    enddo
    cr_at_lis(ifail) = 'FAIL'
+endif
+!
+if(natoms>nprior) then
+   do i=1, natoms
+      if(cr_iscat(i)<0)  then
+         cr_iscat(i)  = 0
+         cr_prop(i)   = 0
+         cr_mole(i)   = 0
+         cr_surf(:,i) = 0
+         cr_magn(:,i) = 0.0
+      endif
+   enddo
 endif
 !
 deallocate(tmp_iscat)
