a8b36d488761d190bf91b5a7f43bddd019e4922f
rneder <reinhard.neder@fau.de>
2022-02-15 17:36:25 +0100

    Moved to PREC_DP double precision

--- discus/prog/quad.f90
+++ discus/prog/quad.f90
@@ -5,26 +5,26 @@ private
 !
 CONTAINS
 !*****7*****************************************************************
-      REAL FUNCTION quad (h, k, rten) 
+!OLD      REAL kind=PREC_DP) FUNCTION quad (h, k, rten) 
 !+                                                                      
 !           Calculates the scalar product of h and k.                   
 !           1/d**2 = h(i)*k(j)*rten(i,j)                                
 !-                                                                      
-      IMPLICIT none 
+!OLD      IMPLICIT none 
 !                                                                       
-      INTEGER, PARAMETER :: idim = 3
+!OLD      INTEGER, PARAMETER :: idim = 3
 !
-      REAL, DIMENSION(3)  , INTENT(IN) :: h
-      REAL, DIMENSION(3)  , INTENT(IN) :: k
-      REAL, DIMENSION(3,3), INTENT(IN) :: rten
+!OLD      REAL(KIND=PREC_DP), DIMENSION(3)  , INTENT(IN) :: h
+!OLD      REAL(KIND=PREC_DP), DIMENSION(3)  , INTENT(IN) :: k
+!OLD      REAL(KIND=PREC_DP), DIMENSION(3,3), INTENT(IN) :: rten
 !                                                                       
-      INTEGER i, j 
+!OLD      INTEGER i, j 
 !                                                                       
-      quad = 0.0 
-      DO i = 1, idim 
-         DO j = 1, idim 
-            quad = quad+h (i) * k (j) * rten (i, j) 
-         ENDDO 
-      ENDDO 
-      END FUNCTION quad                            
+!OLD      quad = 0.0 
+!OLD      DO i = 1, idim 
+!OLD         DO j = 1, idim 
+!OLD            quad = quad+h (i) * k (j) * rten (i, j) 
+!OLD         ENDDO 
+!OLD      ENDDO 
+!OLD      END FUNCTION quad                            
 END MODULE quad_mod
