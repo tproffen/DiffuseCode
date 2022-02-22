118433a92375db1a3dd6fc304eec8a83a478b3ff
rneder <reinhard.neder@fau.de>
2022-02-22 22:29:52 +0100

    Source code cosmetics

--- kuplot/prog/kuplot_low.f90
+++ kuplot/prog/kuplot_low.f90
@@ -5,53 +5,70 @@ module kuplot_low_mod
 contains
 !
 !******7****************************************************************
-      LOGICAL function n_in_f (ini) 
+!
+LOGICAL function n_in_f (ini) 
 !                                                                       
 !     Checks for 2D files in current frame                              
 !                                                                       
-      USE kuplot_config 
-      USE kuplot_mod 
+USE kuplot_config 
+USE kuplot_mod 
 !                                                                       
-      IMPLICIT none 
+IMPLICIT none 
 !                                                                       
-      INTEGER i, ini 
-!      LOGICAL k_in_f 
+integer, intent(out) :: ini
+!
+INTEGER :: i
 !                                                                       
-      n_in_f = .false. 
-      DO i = 1, iz - 1 
-      n_in_f = n_in_f.or. (k_in_f (i) .and.lni (i) ) 
-      IF (n_in_f) ini = i 
-      ENDDO 
+ini = 0
+n_in_f = .false. 
+DO i = 1, iz - 1 
+   n_in_f = n_in_f.or. (k_in_f (i) .and.lni (i) ) 
+   IF(n_in_f) ini = i 
+ENDDO 
 !                                                                       
-      END FUNCTION n_in_f                           
+END FUNCTION n_in_f                           
+!
 !*****7*****************************************************************
-      LOGICAL function k_in_f (ik) 
+!
+LOGICAL function k_in_f (ik) 
 !+                                                                      
 !     Check if data set ik should be plotted in actual frame            
 !-                                                                      
-      USE kuplot_config 
-      USE kuplot_mod 
+USE kuplot_config 
+USE kuplot_mod 
 !                                                                       
-      IMPLICIT none 
+IMPLICIT none 
 !                                                                       
-      INTEGER ik, i 
+integer, intent(in) :: ik
+!
+INTEGER :: i
 !                                                                       
-      k_in_f = .false. 
-      DO i = 1, maxkurvtot 
-      k_in_f = k_in_f.or. (infra (iwin, iframe, i) .eq.ik) 
-      ENDDO 
+k_in_f = .false. 
+DO i = 1, maxkurvtot 
+   k_in_f = k_in_f.or. (infra (iwin, iframe, i) == ik) 
+ENDDO 
 !                                                                       
-      END FUNCTION k_in_f                           
+END FUNCTION k_in_f                           
+!
 !*****7*****************************************************************
-      LOGICAL function inrect (ex, ey, x, y) 
+!
+LOGICAL function inrect (ex, ey, x, y) 
 !-                                                                      
 !       Checks if point x,y is within rectangle ex(2),ey(2).            
 !+                                                                      
-      REAL x, y, ex (2), ey (2) 
+!
+implicit none
+!
+REAL, intent(in) :: x
+REAL, intent(in) :: y
+REAL,  dimension(2), intent(in) :: ex ! (2)
+REAL,  dimension(2), intent(in) :: ey ! (2) 
 !                                                                       
-      inrect = (x.ge.ex (1) .and.x.le.ex (2) ) .and. (y.ge.ey (1)       &
-      .and.y.le.ey (2) )                                                
-      END FUNCTION inrect                           
+inrect = (x >= ex(1) .and. x <= ex(2)) .and.    &
+         (y >= ey(1) .and. y <= ey(2)        )                                                
+!
+END FUNCTION inrect                           
+!
 !***********************************************************************
 !
 end module kuplot_low_mod
