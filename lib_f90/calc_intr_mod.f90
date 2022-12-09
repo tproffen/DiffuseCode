78e7db7b15d588c25817d31b0a060f84d23c0702
rneder <reinhard.neder@fau.de>
2022-12-09 09:07:38 +0100

    Added Pearson Type VII profile function

--- lib_f90/calc_intr_mod.f90
+++ lib_f90/calc_intr_mod.f90
@@ -280,6 +280,9 @@ ELSEIF (lcom.eq.6) then
                   RETURN 
                ENDIF 
             ENDIF
+   elseif (string (ikl - 5:ikl - 1) .eq.'pears') then 
+      CALL intr_pears(line, lp, ww)
+      CALL ersetz2 (string, ikl, iklz, ww, 5, lll) 
    ELSEIF (string (ikl - 5:ikl - 1) .eq.'psvgt') then 
       CALL intr_psvgt(line, lp, ww)
       CALL ersetz2 (string, ikl, iklz, ww, 5, lll) 
@@ -608,6 +611,168 @@ ELSEIF (lcom.eq.4) then
 !
 !*****7************************************************************************* 
 !
+SUBROUTINE intr_pears(line, lp, ww)
+!-
+! Calculate a pseudovoigt function
+!+
+USE ber_params_mod
+USE element_data_mod
+USE errlist_mod
+USE get_params_mod
+USE lib_f90_profile
+USE precision_mod
+USE string_convert_mod
+USE take_param_mod
+USE trig_degree_mod
+USE wink_mod
+!use profile_pointer
+!
+IMPLICIT NONE
+!
+CHARACTER(LEN=*), INTENT(INOUT) :: line
+INTEGER         , INTENT(INOUT) :: lp
+REAL(KIND=PREC_DP), INTENT(OUT)   :: ww
+!
+INTEGER, PARAMETER :: MAXW = 30
+CHARACTER(LEN=MAX(PREC_STRING,LEN(LINE))), DIMENSION(MAXW) :: cpara
+INTEGER                                  , DIMENSION(MAXW) :: lpara
+REAL(KIND=PREC_DP)                       , DIMENSION(MAXW) :: werte
+INTEGER :: ianz          ! number of parameters
+REAL(KIND=PREC_DP)  :: xx
+REAL(KIND=PREC_DP)  :: P_eta
+REAL(KIND=PREC_DP)  :: P_inte
+REAL(KIND=PREC_DP)  :: P_pos
+REAL(KIND=PREC_DP)  :: P_fwhm
+REAL(KIND=PREC_DP)  :: P_asym1
+REAL(KIND=PREC_DP)  :: P_asym2
+REAL(KIND=PREC_DP)  :: itwo           ! Ratio Intensity Ka2/Ka1
+!REAL(KIND=PREC_DP)  :: xw             ! Deviation from position 
+!REAL(KIND=PREC_DP)  :: zz             ! Value for asymmetry function
+!REAL(KIND=PREC_DP)  :: fa             ! Value for asymmetry function
+!REAL(KIND=PREC_DP)  :: fb             ! Value for asymmetry function
+!REAL(KIND=PREC_DP)  :: asym           ! Value for asymmetry function
+LOGICAL             :: axis_theta     ! TRUE==TTH; FALSE=Q
+INTEGER             :: nwave          ! Entry in per_wavel
+CHARACTER  (LEN=4) :: symbol
+REAL(kind=PREC_DP) :: lambda1
+REAL(kind=PREC_DP) :: lambda2
+REAL(kind=PREC_DP) :: lam_1_2         ! Ratio lam(Ka1)/lam(Ka2)
+!
+INTEGER, PARAMETER :: NOPTIONAL = 3
+INTEGER, PARAMETER :: O_ITWO    = 1
+INTEGER, PARAMETER :: O_WAVE    = 2
+INTEGER, PARAMETER :: O_AXIS    = 3
+CHARACTER(LEN=   7)       , DIMENSION(NOPTIONAL) :: oname   !Optional parameter names
+CHARACTER(LEN=PREC_STRING), DIMENSION(NOPTIONAL) :: opara   !Optional parameter strings returned
+INTEGER            , DIMENSION(NOPTIONAL) :: loname  !Lenght opt. para name
+INTEGER            , DIMENSION(NOPTIONAL) :: lopara  !Lenght opt. para name returned
+LOGICAL            , DIMENSION(NOPTIONAL) :: lpresent  !opt. para present
+REAL(KIND=PREC_DP) , DIMENSION(NOPTIONAL) :: owerte   ! Calculated values
+INTEGER, PARAMETER                        :: ncalc = 1 ! Number of values to calculate
+!
+!
+DATA oname  / 'itwo  ' , 'wave  '  ,  'axis   ' /
+DATA loname /  4       ,  4        ,   4      /
+!
+opara  =  (/ '0.500000', 'CU      ',  'tth     '/)   ! Always provide fresh default values
+lopara =  (/  8        ,  2        ,   3        /)
+owerte =  (/  0.500000 ,  1.541800 ,   0.000000 /)
+!
+CALL get_params (line, ianz, cpara, lpara, MAXW, lp) 
+IF(ier_num/=0) RETURN
+!
+IF(ianz>=5) THEN
+   CALL get_optional(ianz, MAXW, cpara, lpara, NOPTIONAL,  ncalc, &
+                     oname, loname, opara, lopara, lpresent, owerte)
+   IF(ier_num/=0) RETURN
+   CALL ber_params(ianz, cpara, lpara, werte, MAXW)
+   IF(ier_num/=0) RETURN
+!
+   xx      = werte(1)
+   P_eta   = werte(2)
+   P_inte  = werte(3)
+   P_pos   = werte(4)
+   P_fwhm  = werte(5)
+   P_asym1 = 0.0D0
+   P_asym2 = 0.0D0
+!
+   IF(ianz>5) P_asym1 = werte(6)
+   IF(ianz>6) P_asym2 = werte(7)
+   CALL do_cap(opara(O_AXIS))
+   IF(opara(O_AXIS)=='TTH') THEN
+      axis_theta = .TRUE.
+   ELSEIF(opara(O_AXIS)=='Q') THEN
+      axis_theta = .FALSE.
+   ELSE
+      ier_num = -6
+      ier_typ = ER_FORT
+      RETURN
+   ENDIF
+   CALL do_cap(opara(O_WAVE))
+   nwave = get_wave_number(opara(O_WAVE))
+   IF(nwave>0) THEN                   ! Found wave length entry
+      CALL get_sym_length(nwave, symbol, lambda1)
+   ELSE
+      cpara(1) = opara(O_WAVE)
+      lpara(1) = lopara(O_WAVE)
+      ianz = 1
+      CALL ber_params(ianz, cpara, lpara, werte, MAXW)
+      IF(ier_num/=0) RETURN
+      lambda1 = werte(1)
+   ENDIF
+!
+   if(P_eta <=0.5D0) then
+      ier_num = -6
+      ier_typ = ER_FORT
+      ier_msg(1) = 'Pearson VII mixing parameter must be > 0.5'
+      ww = 1.0
+      return
+   endif
+   if(P_fwhm <= 0.0) then
+      ier_num = -6
+      ier_typ = ER_FORT
+      ier_msg(1) = 'Pearson VII FWHM parameter must be > 0.0'
+      ww = 1.0
+      return
+   endif
+   if((P_asym1 /= 0.0 .or. P_asym2 /=0.0D0) .and. P_pos<=0.0D0) then
+      ier_num = -6
+      ier_typ = ER_FORT
+      ier_msg(1) = 'Pearson VII POS parameter must be > 0.0'
+      ier_msg(2) = 'if asymmetric profile is used'
+      ww = 1.0
+      return
+   endif
+!
+   ww = pearson_vii(xx, P_eta, P_inte, P_pos, P_fwhm, P_asym1, P_asym2, axis_theta, lambda1)
+!  ww = profile_function(xx, P_eta, P_inte, P_pos, P_fwhm, P_asym1, P_asym2, axis_theta, lambda1)
+   IF(symbol(3:4)=='12') THEN            ! Kalpha 1,2 doublet
+      IF(lpresent(O_ITWO)) THEN          ! User supplied intensity ratios
+         itwo = owerte(O_ITWO)
+      ELSE
+         itwo = get_ka21_inte(nwave)  ! Look up default intensity ratio
+      ENDIF
+      P_inte = P_inte * itwo
+      lam_1_2 = get_ka12_len(nwave)
+      lambda2 = lambda1/lam_1_2
+      IF(axis_theta) THEN                      ! 2Theta axis
+         P_pos = 2.0D0*ASIND(lambda2/2.0D0/(lambda1/2.0D0/SIND(0.5D0*P_pos)))
+      ELSE
+         P_pos = P_pos/lam_1_2
+      ENDIF
+!      P_inte = P_inte * get_ka21_inte(nwave)
+      ww = ww + pearson_vii(xx, P_eta, P_inte, P_pos, P_fwhm, P_asym1, P_asym2, axis_theta, lambda1)
+!     ww = ww + profile_function(xx, P_eta, P_inte, P_pos, P_fwhm, P_asym1, P_asym2, axis_theta, lambda1)
+   ENDIF
+ELSE
+   ier_num = -6
+   ier_typ = ER_FORT
+ENDIF
+!
+END SUBROUTINE intr_pears
+!
+!*****7************************************************************************* 
+!
 SUBROUTINE intr_psvgt(line, lp, ww)
 !-
 ! Calculate a pseudovoigt function
@@ -622,6 +787,7 @@ USE string_convert_mod
 USE take_param_mod
 USE trig_degree_mod
 USE wink_mod
+!use profile_pointer
 !
 IMPLICIT NONE
 !
@@ -718,6 +884,7 @@ IF(ianz>=5) THEN
    ENDIF
 !
    ww = pseudo_voigt(xx, P_eta, P_inte, P_pos, P_fwhm, P_asym1, P_asym2, axis_theta, lambda1)
+!  ww = profile_function(xx, P_eta, P_inte, P_pos, P_fwhm, P_asym1, P_asym2, axis_theta, lambda1)
    IF(symbol(3:4)=='12') THEN            ! Kalpha 1,2 doublet
       IF(lpresent(O_ITWO)) THEN          ! User supplied intensity ratios
          itwo = owerte(O_ITWO)
@@ -734,6 +901,7 @@ IF(ianz>=5) THEN
       ENDIF
 !      P_inte = P_inte * get_ka21_inte(nwave)
       ww = ww + pseudo_voigt(xx, P_eta, P_inte, P_pos, P_fwhm, P_asym1, P_asym2, axis_theta, lambda1)
+!     ww = ww + profile_function(xx, P_eta, P_inte, P_pos, P_fwhm, P_asym1, P_asym2, axis_theta, lambda1)
    ENDIF
 ELSE
    ier_num = -6
@@ -937,3 +1105,90 @@ end subroutine intr_tukey
 !*****7************************************************************************* 
 !
 !END MODULE calc_intr_mod
+!
+module profile_pointer
+!
+contains
+!
+subroutine set_profile_pointer(iprofile)
+!-
+!  Set the pointer to the chosen profile function
+!+
+use precision_mod
+!
+integer, intent(in) :: iprofile
+!
+integer, parameter :: PSEUDO = 1
+integer, parameter :: PEARS  = 2
+!
+interface
+   real(kind=PREC_DP) function profile_function(xx, P_eta, P_inte, P_pos,     &
+                         P_fwhm, P_asym1, P_asym2, &
+                         axis, lambda) result(ww)
+!
+   use precision_mod
+!
+   REAL(KIND=PREC_DP), INTENT(IN)  :: xx            ! Position at which to calculate Pseduovoigt
+   REAL(KIND=PREC_DP), INTENT(IN)  :: P_eta         ! Profile Mixing parameter
+   REAL(KIND=PREC_DP), INTENT(IN)  :: P_inte        ! Integrated intensity
+   REAL(KIND=PREC_DP), INTENT(IN)  :: P_pos         ! Position
+   REAL(KIND=PREC_DP), INTENT(IN)  :: P_fwhm        ! FWHM
+   REAL(KIND=PREC_DP), INTENT(IN)  :: P_asym1       ! Asymmetry parameter 1
+   REAL(KIND=PREC_DP), INTENT(IN)  :: P_asym2       ! Asymmetry parameter 2
+   LOGICAL           , INTENT(IN)  :: axis          ! TRUE if TTH Scale
+   REAL(KIND=PREC_DP), INTENT(IN)  :: lambda        ! Wave length
+!
+   end function profile_function
+end interface
+!
+interface
+   real(kind=PREC_DP) function pseudo_voigt(xx, P_eta, P_inte, P_pos,     &
+                         P_fwhm, P_asym1, P_asym2, &
+                         axis, lambda) result(ww)
+!
+   use precision_mod
+!
+   REAL(KIND=PREC_DP), INTENT(IN)  :: xx            ! Position at which to calculate Pseduovoigt
+   REAL(KIND=PREC_DP), INTENT(IN)  :: P_eta         ! Profile Mixing parameter
+   REAL(KIND=PREC_DP), INTENT(IN)  :: P_inte        ! Integrated intensity
+   REAL(KIND=PREC_DP), INTENT(IN)  :: P_pos         ! Position
+   REAL(KIND=PREC_DP), INTENT(IN)  :: P_fwhm        ! FWHM
+   REAL(KIND=PREC_DP), INTENT(IN)  :: P_asym1       ! Asymmetry parameter 1
+   REAL(KIND=PREC_DP), INTENT(IN)  :: P_asym2       ! Asymmetry parameter 2
+   LOGICAL           , INTENT(IN)  :: axis          ! TRUE if TTH Scale
+   REAL(KIND=PREC_DP), INTENT(IN)  :: lambda        ! Wave length
+!
+   end function pseudo_voigt
+end interface
+!
+interface
+   real(kind=PREC_DP) function pearson_vii (xx, P_eta, P_inte, P_pos,     &
+                         P_fwhm, P_asym1, P_asym2, &
+                         axis, lambda) result(ww)
+!
+   use precision_mod
+!
+   REAL(KIND=PREC_DP), INTENT(IN)  :: xx            ! Position at which to calculate Pseduovoigt
+   REAL(KIND=PREC_DP), INTENT(IN)  :: P_eta         ! Profile Mixing parameter
+   REAL(KIND=PREC_DP), INTENT(IN)  :: P_inte        ! Integrated intensity
+   REAL(KIND=PREC_DP), INTENT(IN)  :: P_pos         ! Position
+   REAL(KIND=PREC_DP), INTENT(IN)  :: P_fwhm        ! FWHM
+   REAL(KIND=PREC_DP), INTENT(IN)  :: P_asym1       ! Asymmetry parameter 1
+   REAL(KIND=PREC_DP), INTENT(IN)  :: P_asym2       ! Asymmetry parameter 2
+   LOGICAL           , INTENT(IN)  :: axis          ! TRUE if TTH Scale
+   REAL(KIND=PREC_DP), INTENT(IN)  :: lambda        ! Wave length
+!
+   end function pearson_vii 
+end interface
+!
+procedure(profile_function), pointer :: p_profile_function => NULL()
+!
+if(iprofile==PSEUDO) then
+   p_profile_function => pseudo_voigt
+elseif(iprofile==PEARS ) then
+   p_profile_function => pearson_vii
+endif
+!
+end subroutine set_profile_pointer
+!
+end module profile_pointer
--- lib_f90/lib_f90_profile.f90
+++ lib_f90/lib_f90_profile.f90
@@ -66,6 +66,63 @@ END FUNCTION pseudo_voigt
 !
 !*******************************************************************************
 !
+real(kind=PREC_DP) function pearson_vii(xx, P_m, P_inte, P_pos,     &
+                                        P_fwhm, P_asym1, P_asym2,   &
+                                        axis, lambda) RESULT(ww)
+!-
+! Caclulate a Pearson Type VII function 
+!+
+!
+use precision_mod
+use gamma_mod
+use wink_mod
+!
+implicit none
+!
+real(KIND=PREC_DP), INTENT(IN)  :: xx            ! Position at which to calculate Pseduovoigt
+real(KIND=PREC_DP), INTENT(IN)  :: P_m           ! Profile Mixing parameter
+real(KIND=PREC_DP), INTENT(IN)  :: P_inte        ! Integrated intensity
+real(KIND=PREC_DP), INTENT(IN)  :: P_pos         ! Position
+real(KIND=PREC_DP), INTENT(IN)  :: P_fwhm        ! FWHM
+REAL(KIND=PREC_DP), INTENT(IN)  :: P_asym1       ! Asymmetry parameter 1
+REAL(KIND=PREC_DP), INTENT(IN)  :: P_asym2       ! Asymmetry parameter 2
+LOGICAL           , INTENT(IN)  :: axis          ! TRUE if TTH Scale
+REAL(KIND=PREC_DP), INTENT(IN)  :: lambda        ! Wave length
+!
+real(kind=PREC_DP) :: alpha
+!
+REAL(KIND=PREC_DP)  :: xw             ! Deviation from position 
+REAL(KIND=PREC_DP)  :: zz             ! Value for asymmetry function
+REAL(KIND=PREC_DP)  :: fa             ! Value for asymmetry function
+REAL(KIND=PREC_DP)  :: fb             ! Value for asymmetry function
+REAL(KIND=PREC_DP)  :: asym           ! Value for asymmetry function
+!
+!lpha = P_fwhm *0.5D0 / sqrt(0.5D0**(-1.0D0/P_m) - 1.0D0)
+alpha = P_fwhm *0.5D0 / sqrt(2.0D0**( 1.0D0/P_m) - 1.0D0)
+!
+xw = xx - P_pos                 ! Deviation from position
+zz = xw / P_fwhm
+fa = 2.D0 * zz * EXP( -zz**2)
+fb = 2.D0 * (2.D0 * zz**2 - 3.) * fa
+asym = 1.0D0
+if(P_asym1/=0.0D0 .or. P_asym2/=0.0D0) then
+IF(axis) THEN
+      asym = asym + (P_asym1*fa + P_asym2*fb) / tanh(RAD*0.5*P_pos )
+ELSE
+      asym = asym + (P_asym1*fa + P_asym2*fb) / tanh(asin(P_pos/FPI/lambda))
+ENDIF
+endif
+!
+ww = P_inte * ( (1.0D0+((xx-P_pos)/alpha)**2)**(-P_m) )/ &
+              (alpha*func_beta(P_m-0.5D0, 0.5D0)) * asym
+!write(*,'(7f10.4)') xx, P_pos, ( (1.0D0+((xx-P_pos)/alpha)**2)**(-P_m) )/ &
+!              (alpha*func_beta(P_m-0.5D0, 0.5D0)), (alpha*func_beta(P_m-0.5D0, 0.5D0)), &
+!              P_m, P_fwhm, alpha
+!
+end function pearson_vii
+!
+!*******************************************************************************
+!
 real(kind=PREC_DP) function tukey(x, alpha, width)
 !-
 !  Return a value for the Tukey window function
