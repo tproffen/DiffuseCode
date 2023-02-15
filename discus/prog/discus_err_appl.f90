f93cb409539aeae6f3ff6af26b14d2a4a75af5bb
rneder <reinhard.neder@fau.de>
2023-02-15 07:19:31 +0100

    Updated

--- discus/prog/discus_err_appl.f90
+++ discus/prog/discus_err_appl.f90
@@ -13,11 +13,12 @@ USE lib_errlist_func
 !
 !
       integer       iu,io
-      PARAMETER    (IU=-188,IO=7)
+      PARAMETER    (IU=-189,IO=7)
 !
       CHARACTER(LEN=45) ::  ERROR(IU:IO)
 !
       DATA ERROR ( IU : -181) /                          &
+     &  'Parameter name on ''atom'' line is invalid   ', & !-189 ! DISCUS    
      &  'Reciprocal grid does not include zero point  ', & !-188 ! DISCUS    
      &  '1./Increment vector /= N                     ', & !-187 ! DISCUS    
      &  'Increment vector /= N/crystal_dimension      ', & !-186 ! DISCUS
