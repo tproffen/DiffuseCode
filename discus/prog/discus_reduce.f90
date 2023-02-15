4cf58521dde8be8d342078ddd5adcd7e8eac070a
rneder <reinhard.neder@fau.de>
2023-02-15 07:28:00 +0100

    New routine to reduce nuber of different atom types

---
+++ discus/prog/discus_reduce.f90
@@ -0,0 +1,93 @@
+module reduce_atoms_mod
+!
+!  Routines to modify the number of atom/molecule types on the fly
+!-
+private
+!
+public reduce_atoms
+!
+contains
+!
+!*******************************************************************************
+!
+subroutine reduce_atoms(MAXMASK, uni_mask)
+!-
+! Reduce the number of atom types according to the uniqe mask
+!+
+!
+use crystal_mod
+use atom_line_mod
+!
+use precision_mod
+!
+implicit none
+!
+integer                      , intent(in) :: MAXMASK   ! Array dimension
+logical, dimension(0:MAXMASK), intent(in) :: uni_mask  ! The unique mask
+!
+integer :: lb
+integer :: ub
+!
+character(len=4)  , dimension(:), allocatable :: at_lis
+real(kind=PREC_DP), dimension(:), allocatable :: at_dw
+real(kind=PREC_DP), dimension(:), allocatable :: at_occ
+!
+character(len=4)                     :: nw_name
+real(kind=PREC_DP)                   :: nw_dw
+real(kind=PREC_DP)                   :: nw_occ
+!
+integer :: i,j   ! Loop indices
+integer :: is_type ! Derived atom type
+integer :: nscat ! number atom types as to unique mask
+integer :: nscat_start
+!
+lb = lbound(cr_at_lis,1)
+ub = ubound(cr_at_lis,1)
+allocate(at_lis (lb:ub))
+allocate(at_dw  (lb:ub))
+allocate(at_occ (lb:ub))
+!
+if(lb==0) then
+   at_lis(lb) = 'VOID'
+   at_dw(lb)  = 0.0D0
+   at_occ(lb) = 1.0D0
+endif
+!
+nscat = 0
+nscat_start = lb
+!
+do i=1, cr_natoms
+   nw_name = cr_at_lis(cr_iscat(i))         ! Use present atom name
+   nw_dw   = cr_dw(cr_iscat(i))             ! use present ADP
+   nw_occ  = cr_occ(cr_iscat(i))            ! use present ADP
+   is_type = atom_get_type(MAXSCAT, nscat_start, nscat, MAXMASK,      &
+                       at_lis, at_dw, at_occ,        &
+                       nw_name, nw_dw, nw_occ, uni_mask)
+   if(is_type==-1) then                     ! New atom type
+      nscat = nscat + 1
+      at_lis(nscat) = nw_name
+      at_dw (nscat) = nw_dw
+      at_occ(nscat) = nw_occ
+      is_type = nscat
+   endif
+   cr_iscat(i) = is_type
+enddo
+!
+cr_nscat = nscat
+cr_at_lis = at_lis
+cr_dw     = at_dw
+cr_occ    = at_occ
+as_at_lis = at_lis
+as_dw     = at_dw
+as_occ    = at_occ
+!
+deallocate(at_lis)
+deallocate(at_dw )
+deallocate(at_occ)
+!
+end subroutine reduce_atoms
+!
+!
+!*******************************************************************************
+!
+end module reduce_atoms_mod
