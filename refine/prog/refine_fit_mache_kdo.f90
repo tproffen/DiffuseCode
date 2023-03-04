d458039d9560a49d3c1aa9f2b7851b2256fba0ea
rneder <reinhard.neder@fau.de>
2023-03-04 12:06:18 +0100

    Source code cosmetics preparation for str_comp

--- refine/prog/refine_fit_mache_kdo.f90
+++ refine/prog/refine_fit_mache_kdo.f90
@@ -57,8 +57,8 @@ ENDIF
 !
 IF (indxg /= 0                                            &
    .AND..NOT. (str_comp (befehl, 'echo',   2, lbef, 4) )    &
+   .AND..NOT. (str_comp (befehl, 'system', 2, lbef, 6) )    &
    .AND..NOT. (str_comp (befehl, 'help',   2, lbef, 4) .OR. &
-               str_comp (befehl, 'system', 2, lbef, 6) .OR. &
                str_comp (befehl, '?   ',   2, lbef, 4) )    &
    .AND. INDEX(line,'==') == 0                          )THEN
    CALL do_math (line, indxg, length)
