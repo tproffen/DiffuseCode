4b30604181638993639ac8123e7638002732c5e6
rneder <reinhard.neder@fau.de>
2023-03-10 16:58:32 +0100

    Added a 'nipl' write

--- lib_f90/lib_write_debug.f90
+++ lib_f90/lib_write_debug.f90
@@ -4,7 +4,7 @@ module lib_write_mod
 !+
 !
 interface tofile
-  module procedure complex_tofile, real_tofile
+  module procedure complex_tofile, real_tofile, real_tonipl
 end interface tofile
 !
 private
@@ -61,6 +61,35 @@ enddo
 close(unit=66)
 !
 end subroutine real_tofile
+!
+!*******************************************************************************
+!
+subroutine real_tonipl(idims, cname, field, xmin, xstep)
+!-
+!  Real Version 2D field to NIPL type output
+!+
+!
+use precision_mod
+!
+integer           , dimension(2)      , intent(in) :: idims
+character(len=*)                      , intent(in) :: cname
+real(kind=PREC_DP), dimension(idims(1), idims(2)), intent(in) :: field
+real(kind=PREC_DP), dimension(2)      , intent(in) :: xmin
+real(kind=PREC_DP), dimension(2)      , intent(in) :: xstep
+!
+integer :: i, j
+!
+!
+open(unit=66, file=cname, status='unknown')
+write(66, '(2i10)') idims
+write(66, '(4(f15.4,2x))') xmin(1), xmin(1) + (idims(1)-1)*xstep(1), &
+                           xmin(2), xmin(2) + (idims(2)-1)*xstep(2)
+do j=1, idims(2)
+   write(66, '(5(g15.8,2x))') (field(i,j), i=1, idims(1))
+enddo
+close(unit=66)
+!
+end subroutine real_tonipl
 !
 !###############################################################################
 !
