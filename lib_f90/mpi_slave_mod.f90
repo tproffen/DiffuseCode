b720c6aa1fe623947f1a22d0dcc1fb5a2024c84e
rneder <reinhard.neder@fau.de>
2023-05-27 12:38:09 +0200

    Improved error comminucation DIFFEV slave to master

--- lib_f90/mpi_slave_mod.f90
+++ lib_f90/mpi_slave_mod.f90
@@ -3,5 +3,7 @@ MODULE mpi_slave_mod
 LOGICAL  :: mpi_active   = .false.
 LOGICAL  :: mpi_is_slave = .false.
 INTEGER  :: mpi_slave_error = 0
+INTEGER  :: mpi_slave_err_typ = 0
+character(len=80), dimension(7) :: mpi_slave_msg = ' '
 !
 END MODULE mpi_slave_mod
