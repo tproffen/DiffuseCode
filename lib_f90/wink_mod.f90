cf9f18adba7e3584aff72bfca7f7156296bc1542
rneder <reinhard.neder@fau.de>
2023-06-12 15:21:45 +0200

    Source code cosmetics

--- lib_f90/wink_mod.f90
+++ lib_f90/wink_mod.f90
@@ -1,16 +1,16 @@
-MODULE wink_mod
+module wink_mod
 !+
 !     This file the definitions for radian, 2 pi 
 !-
-   USE precision_mod
-   IMPLICIT NONE
-   PUBLIC
-   SAVE
+use precision_mod
+implicit none
+public
+save
 !
-   REAL(PREC_DP), PARAMETER :: pi  = 3.1415926535897932384626433832795028841971693993751D0
-   REAL(PREC_DP), PARAMETER :: zpi = 2.0D0 * pi
-   REAL(PREC_DP), PARAMETER :: fpi = 4.0D0 * pi
-   REAL(PREC_DP), PARAMETER :: rad = pi/180.D0
+real(PREC_DP), parameter :: pi  = 3.1415926535897932384626433832795028841971693993751D0
+real(PREC_DP), parameter :: zpi = 2.0D0 * pi
+real(PREC_DP), parameter :: fpi = 4.0D0 * pi
+real(PREC_DP), parameter :: rad = pi/180.D0
 !
 !
-END MODULE wink_mod
+end module wink_mod
