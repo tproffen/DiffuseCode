e9f4a0d0d9083d3660cb12e7faa2a0aaa0e09ff8
rneder <reinhard.neder@fau.de>
2023-12-20 17:14:57 +0100

    Further development for Anisotropic ADP

--- discus/prog/discus_prep_anis.f90
+++ discus/prog/discus_prep_anis.f90
@@ -4,6 +4,18 @@ module prep_anis_mod
 !   Determine eigenvectors and Eigenvalues from U_ij
 !   Apply symmetry to Eigenvectors for all sites
 !+
+private
+public prep_anis
+public lookup_anis
+public calc_prin
+!
+interface lookup_anis
+   module procedure lookup_anis_6, lookup_anis_3x3
+end interface lookup_anis
+!
+interface calc_prin
+   module procedure calc_prin_6, calc_prin_3x3
+end interface calc_prin
 !
 contains
 !
@@ -28,18 +40,12 @@ integer :: itype    ! Loop index atom types
 integer :: iatom    ! Loop index atoms
 integer :: j, l, m, n  ! Dummy loop indices
 integer :: neigen      ! Number distinct eigenvectors
+logical :: lsuccess    ! Success in subroutines
 real(kind=PREC_DP), dimension(3,3) :: uij   ! U_ij in crystal dimensions
 real(kind=PREC_DP), dimension(3,3) :: uijs  ! U_ij in crystal dimensions after symmetry
 real(kind=PREC_DP), dimension(3,3) :: ucij   ! U_ij in cartesian coordinates
-real(kind=PREC_DP), dimension(3)   :: eigen_val   ! U_ij in cartesian coordinates
-real(kind=PREC_DP), dimension(3,3) :: eigen_car   ! Eigenvectors in cartesian coordinates
-real(kind=PREC_DP), dimension(3,3) :: eigen_cry   ! Eigenvectors in crystal space 
 real(kind=PREC_DP), dimension(3,3) :: symm_mat    ! Symmetry matrix
 real(kind=PREC_DP), dimension(3,3) :: symm_imat    ! Inverse Symmetry matrix
-real(kind=PREC_DP), DIMENSION(3  ) ::      vector
-real(kind=PREC_DP), DIMENSION(3  ) ::      u, v, w
-real(kind=PREC_DP), DIMENSION(3,3)          ::      imat     = &
-   reshape((/1.0D0,0.0D0,0.0D0, 0.0D0,1.0D0,0.0D0, 0.0D0,0.0D0,1.0D0/),SHAPE(imat))
 !
 !write(*,*) ' PREP_ANIS cr_anis  ', ubound(cr_anis)
 !write(*,*) ' PREP_ANIS cr_anis_f', ubound(cr_anis_full)
@@ -56,12 +62,16 @@ cr_prin      = 0.0D0
 ! Step 0:  Determine number of different atoms for a given scattering type
 !
 j = 0
+lsuccess= .false.
 cr_ndiffer = 0
 cr_nanis = 0
-if(maxval(abs(cr_anis(:,:)))<TOL) then
-   cr_iscat(1:cr_ncatoms,3) = 1
-   return
-endif
+!if(maxval(abs(cr_anis(:,:)))<TOL) then
+!   cr_iscat(1:cr_ncatoms,3) = 1
+!   return
+!endif
+!do iatom=1,cr_ncatoms
+!write(*,'( i3, 6f10.6)') iatom, cr_anis(:,iatom)
+!enddo
 do iatom=1,cr_ncatoms
    l = cr_iscat(iatom,1)        ! Current atom type
    cr_ndiffer(l) = cr_ndiffer(l) + 1
@@ -69,10 +79,12 @@ enddo
 !
 ! Step 1:  Determine Eigenvalues and Eigenvectors 
 loop_atoms:do iatom=1,cr_ncatoms 
+   j = 0
+   lsuccess= .false.
    itype = cr_iscat(iatom,1)
    cr_ianis(iatom) = iatom
 !  write(*,*)
-!  write(*,'(a,i3,2i3, 3f7.3, i4)') ' ATOM ', iatom, itype, cr_ianis(iatom), cr_pos(:,iatom), cr_is_sym(iatom)
+!  write(*,'(a,i3,2i3, 3f7.3, i4)') ' ATOM ', iatom, itype, cr_iscat(iatom,3), cr_pos(:,iatom), cr_is_sym(iatom)
 !  write(*,'(a,        6f7.3    )') ' UANI ', cr_anis(:,itype)
    if(maxval(abs(cr_anis(:,itype)))> 0.0D0) then
       uij(1,1) = cr_anis(1, itype)
@@ -87,10 +99,20 @@ loop_atoms:do iatom=1,cr_ncatoms
       cr_iscat(iatom, 3) = cr_is_sym(iatom)
    else
       ucij      = 0.0D0
-      ucij(1,1) = cr_dw(itype)/8.0D0/pi**2
+      if(cr_dw(itype)>0.0_PREC_DP) then
+      ucij(1,1) = cr_dw(itype)/8.0D0/pi**2    ! WORK !?!??!
       ucij(2,2) = cr_dw(itype)/8.0D0/pi**2
       ucij(3,3) = cr_dw(itype)/8.0D0/pi**2
+      else
+      ucij(1,1) = 1.0/8.0D0/pi**2
+      ucij(2,2) = 1.0/8.0D0/pi**2
+      ucij(3,3) = 1.0/8.0D0/pi**2
+      endif
       uij = matmul((cr_dimat), matmul(ucij, transpose(cr_dimat)))
+!      uij       = 0.0_PREC_DP
+!     uij (1,1) = cr_dw(itype)/8.0D0/pi**2
+!     uij (2,2) = cr_dw(itype)/8.0D0/pi**2
+!     uij (3,3) = cr_dw(itype)/8.0D0/pi**2
       cr_iscat(iatom, 3) = 1
 !    write(*,*) 
 !    write(*,'(a,2(2x,3(2x,f9.6)))') 'UC_ij ', uij(1,:), ucij(1,:)
@@ -108,80 +130,244 @@ loop_atoms:do iatom=1,cr_ncatoms
       uij = uijs
    endif 
 !
-   do j=1, cr_nanis
-      if(abs(cr_anis_full(1,j)-uij(1,1))<TOL .and.  &
-         abs(cr_anis_full(2,j)-uij(2,2))<TOL .and.  &
-         abs(cr_anis_full(3,j)-uij(3,3))<TOL .and.  &
-         abs(cr_anis_full(4,j)-uij(3,2))<TOL .and.  &
-         abs(cr_anis_full(5,j)-uij(3,1))<TOL .and.  &
-         abs(cr_anis_full(6,j)-uij(2,1))<TOL ) then  
+!write(*,'(a,i3, 3f10.6)') ' Uij  Row', 1, uij(1,:)
+!write(*,'(a,i3, 3f10.6)') ' Uij  Row', 2, uij(2,:)
+!write(*,'(a,i3, 3f10.6)') ' Uij  Row', 3, uij(3,:)
+   call lookup_anis(cr_nanis, cr_anis_full, cr_prin, uij, j, lsuccess)
+!write(*,'(i3, 6f10.6)') j, cr_anis_full(:,j)
+   cr_iscat(iatom,3) = j            ! Assign ADP type
+   if(lsuccess) then
+      cycle loop_atoms
+   endif
+!
+   call calc_prin(cr_iscat(iatom, 3), cr_nanis, uij, cr_dmat, cr_emat, cr_gten, cr_prin)
+enddo loop_atoms
+!
+!cr_nanis = cr_ncatoms
+!write(*,*) ' cr_anis_full ', ubound(cr_anis_full)
+!write(*,*) ' cr_prin      ', ubound(cr_prin     )
+!do j=1, cr_nanis
+!write(*,'(i3, 6f10.6)') j, cr_anis_full(:,j)
+!enddo
+!write(*,*)
+!do j=1, cr_nanis
+!write(*,'(a,3f9.5,a,f9.5,a,f9.5)') 'E(1,0,0) => ', cr_prin(1,1:3,j),  ' VAL ', cr_prin(1,4,j)
+!write(*,'(a,3f9.5,a,f9.5,a,f9.5)') 'E(0,1,0) => ', cr_prin(2,1:3,j),  ' VAL ', cr_prin(2,4,j)
+!write(*,'(a,3f9.5,a,f9.5,a,f9.5)') 'E(0,0,1) => ', cr_prin(3,1:3,j),  ' VAL ', cr_prin(3,4,j)
+!write(*,*)
+!enddo
+!
+!
+end subroutine prep_anis
+!
+!*******************************************************************************
+!
+subroutine lookup_anis_6(cr_nanis, cr_anis_full,  cr_prin, uij_6, ientry, lsuccess)
+!-
+!  Check if Uij are different from any cr_anis_full entry, if so set new entry
+!  Version for Uij in SHELX style
+!+
+!
+use allocate_generic
+use precision_mod
+!
+implicit none
+!
+integer                                          , intent(inout) :: cr_nanis
+real(kind=PREC_DP), dimension(:,:)  , allocatable, intent(inout) :: cr_anis_full
+real(kind=PREC_DP), dimension(:,:,:), allocatable, intent(inout) :: cr_prin
+real(kind=PREC_DP), dimension(6)                 , intent(in)    :: uij_6
+integer                                          , intent(out)   :: ientry
+logical                                          , intent(out)   :: lsuccess
+!
+real(kind=PREC_DP), dimension(3,3) :: uij
+uij(1,1) = uij_6(1)
+uij(2,2) = uij_6(2)
+uij(3,3) = uij_6(3)
+uij(3,2) = uij_6(4)
+uij(2,3) = uij_6(4)
+uij(3,1) = uij_6(5)
+uij(1,3) = uij_6(5)
+uij(2,1) = uij_6(6)
+uij(1,2) = uij_6(6)
+call lookup_anis_3x3(cr_nanis, cr_anis_full,  cr_prin, uij, ientry, lsuccess)
+!
+end subroutine lookup_anis_6
+!
+!*******************************************************************************
+!
+subroutine lookup_anis_3x3(cr_nanis, cr_anis_full,  cr_prin, uij, ientry, lsuccess)
+!-
+!  Check if Uij are different from any cr_anis_full entry, if so set new entry
+!  Version for Uij in as 3x3 matrix
+!+
+!
+use allocate_generic
+use precision_mod
+!
+implicit none
+!
+integer                                          , intent(inout) :: cr_nanis
+real(kind=PREC_DP), dimension(:,:)  , allocatable, intent(inout) :: cr_anis_full
+real(kind=PREC_DP), dimension(:,:,:), allocatable, intent(inout) :: cr_prin
+real(kind=PREC_DP), dimension(3,3)               , intent(in)    :: uij
+integer                                          , intent(out)   :: ientry
+logical                                          , intent(out)   :: lsuccess
+!
+real(kind=PREC_DP), parameter :: TOL = 1.0D-6
+!
+integer :: j 
+integer :: all_status
+!
+ientry = -1
+lsuccess = .FALSE.
+!
+loop_nanis: do j=1, cr_nanis
+   if(&
+     (abs(cr_anis_full(1,j)-uij(1,1))<TOL .and.  &
+      abs(cr_anis_full(2,j)-uij(2,2))<TOL .and.  &
+      abs(cr_anis_full(3,j)-uij(3,3))<TOL .and.  &
+      abs(cr_anis_full(4,j)-uij(3,2))<TOL .and.  &
+      abs(cr_anis_full(5,j)-uij(3,1))<TOL .and.  &
+      abs(cr_anis_full(6,j)-uij(2,1))<TOL      ) &
+      .or.                                     &
+     (abs(cr_anis_full(1,j)+uij(1,1))<TOL .and.  &
+      abs(cr_anis_full(2,j)+uij(2,2))<TOL .and.  &
+      abs(cr_anis_full(3,j)+uij(3,3))<TOL .and.  &
+      abs(cr_anis_full(4,j)+uij(3,2))<TOL .and.  &
+      abs(cr_anis_full(5,j)+uij(3,1))<TOL .and.  &
+      abs(cr_anis_full(6,j)+uij(2,1))<TOL      ) &
+      ) then  
+      ientry = j
+      lsuccess = .TRUE.
+   exit loop_nanis
 !WORK         cr_iscat(iatom,3) = j            ! Assign ADP type
 !WORK         cycle loop_atoms
-      endif
-   enddo
-   cr_nanis = cr_nanis + 1
-   cr_anis_full(1,iatom) = uij(1,1)
-   cr_anis_full(2,iatom) = uij(2,2)
-   cr_anis_full(3,iatom) = uij(3,3)
-   cr_anis_full(4,iatom) = uij(3,2)
-   cr_anis_full(5,iatom) = uij(3,1)
-   cr_anis_full(6,iatom) = uij(2,1)
-   ucij = 0.0D0
-!     do j=1, 3
-!       do l=1, 3
-!          do m=1, 3
-!             do n=1, 3
-!                ucij(j,l) = ucij(j,l) + cr_dmat(j,m)*cr_dmat(l,n)*uij(m,n)
-!             enddo
-!          enddo
-!       enddo
-!    enddo
-!    write(*,'(a,i4)') 'Atom type ', itype
-!    write(*,'(a,2(2x,3(2x,f9.6)))') 'UC_ij ', uij(1,:), ucij(1,:)
-!    write(*,'(a,2(2x,3(2x,f9.6)))') 'UC_ij ', uij(2,:), ucij(2,:)
-!    write(*,'(a,2(2x,3(2x,f9.6)))') 'UC_ij ', uij(3,:), ucij(3,:)
+   endif
+enddo loop_nanis
 !
-     ucij = matmul((cr_dmat), matmul(uij, transpose(cr_dmat)))
+if(ientry==-1) then   ! new entry
+   cr_nanis = cr_nanis + 1
+   ientry   = cr_nanis
+   if(cr_nanis>=ubound(cr_anis_full,2)) then
+      call alloc_arr(cr_anis_full, 1,6, 1, cr_nanis, all_status, 0.0D0      )
+      call alloc_arr(cr_prin, 1, 3,1,4, 1, cr_nanis, all_status, 0.0D0      )
+   endif
+   cr_anis_full(1,ientry) = uij(1,1)
+   cr_anis_full(2,ientry) = uij(2,2)
+   cr_anis_full(3,ientry) = uij(3,3)
+   cr_anis_full(4,ientry) = uij(3,2)
+   cr_anis_full(5,ientry) = uij(3,1)
+   cr_anis_full(6,ientry) = uij(2,1)
+endif
+!
+end subroutine lookup_anis_3x3
+!
+!*******************************************************************************
+!
+subroutine calc_prin_6(ientry, nanis, uij_6, dmat, emat, gten, prin)
+!-
+! Calculate principal axis for Uij
+!-
+!
+use errlist_mod
+use lib_metric_mod
+use math_sup, only:eigen_value, test_eigen_value
+use precision_mod
+!
+implicit none
+!
+integer                               , intent(in)  :: ientry
+integer                               , intent(in)  :: nanis
+real(kind=PREC_DP), dimension(6)      , intent(in)  :: uij_6
+real(kind=PREC_DP), dimension(3,3)    , intent(in)  :: dmat
+real(kind=PREC_DP), dimension(3,3)    , intent(in)  :: emat
+real(kind=PREC_DP), dimension(3,3)    , intent(in)  :: gten
+real(kind=PREC_DP), dimension(3,4,nanis), intent(out) :: prin
+!
+real(kind=PREC_DP), dimension(3,3) :: uij
+!
+uij(1,1) = uij_6(1)
+uij(2,2) = uij_6(2)
+uij(3,3) = uij_6(3)
+uij(3,2) = uij_6(4)
+uij(2,3) = uij_6(4)
+uij(3,1) = uij_6(5)
+uij(1,3) = uij_6(5)
+uij(2,1) = uij_6(6)
+uij(1,2) = uij_6(6)
+!
+call calc_prin_3x3(ientry, nanis, uij, dmat, emat, gten, prin)
+!
+end subroutine calc_prin_6
+!
+!*******************************************************************************
+!
+subroutine calc_prin_3x3(ientry, nanis, uij, dmat, emat, gten, prin)
+!-
+! Calculate principal axis for Uij
+!-
+!
+use errlist_mod
+use lib_metric_mod
+use math_sup, only:eigen_value, test_eigen_value
+use precision_mod
+!
+implicit none
+!
+integer                               , intent(in)  :: ientry
+integer                               , intent(in)  :: nanis
+real(kind=PREC_DP), dimension(3,3)    , intent(in)  :: uij
+real(kind=PREC_DP), dimension(3,3)    , intent(in)  :: dmat
+real(kind=PREC_DP), dimension(3,3)    , intent(in)  :: emat
+real(kind=PREC_DP), dimension(3,3)    , intent(in)  :: gten
+real(kind=PREC_DP), dimension(3,4,nanis), intent(out) :: prin
+!
+integer :: neigen
+real(kind=PREC_DP), dimension(3)   :: eigen_val   ! U_ij in cartesian coordinates
+real(kind=PREC_DP), dimension(3,3) :: eigen_car   ! Eigenvectors in cartesian coordinates
+real(kind=PREC_DP), DIMENSION(3  ) ::      vector
+real(kind=PREC_DP), DIMENSION(3  ) ::      u, v, w
+real(kind=PREC_DP), dimension(3,3) :: ucij
+real(kind=PREC_DP), DIMENSION(3,3)          ::      imat     = &
+   reshape((/1.0D0,0.0D0,0.0D0, 0.0D0,1.0D0,0.0D0, 0.0D0,0.0D0,1.0D0/),SHAPE(imat))
+!
+ucij = matmul((dmat), matmul(uij, transpose(dmat)))
 !     write(*,*) 
 !     write(*,'(a,2(2x,3(2x,f9.6)))') 'UC_ij ', uij(1,:), ucij(1,:)
 !     write(*,'(a,2(2x,3(2x,f9.6)))') 'UC_ij ', uij(2,:), ucij(2,:)
 !     write(*,'(a,2(2x,3(2x,f9.6)))') 'UC_ij ', uij(3,:), ucij(3,:)
-!    ucij = matmul((cr_dimat), matmul(uij, transpose(cr_dimat)))
+!    ucij = matmul((dimat), matmul(uij, transpose(dimat)))
 !    write(*,*) 
 !    write(*,'(a,2(2x,3(2x,f9.6)))') 'UC_ij ', uij(1,:), ucij(1,:)
 !    write(*,'(a,2(2x,3(2x,f9.6)))') 'UC_ij ', uij(2,:), ucij(2,:)
 !    write(*,'(a,2(2x,3(2x,f9.6)))') 'UC_ij ', uij(3,:), ucij(3,:)
-     call eigen_value(ucij, eigen_val, eigen_car, imat, neigen)
+call eigen_value(ucij, eigen_val, eigen_car, imat, neigen)
+prin  (1,1:3, ientry) = eigen_car(:,1)
+prin  (2,1:3, ientry) = eigen_car(:,2)
+prin  (3,1:3, ientry) = eigen_car(:,3)
+prin  (:,4  , ientry) = eigen_val
 !write(*,*) ' GOT EIGENVALUES ', ier_num, ier_typ
 !write(*,*) ' GOT EIGENVALUES ', eigen_val, neigen
 !write(*,*) ' GOT EIGENVECTOR ', eigen_car(:,1)
 !write(*,*) ' GOT EIGENVECTOR ', eigen_car(:,2)
 !write(*,*) ' GOT EIGENVECTOR ', eigen_car(:,3)
+!debug
 !     call test_eigen_value(ucij, eigen_val, eigen_car, imat )
-vector = eigen_car(:,1)
-u = matmul((cr_emat), vector)
-vector = eigen_car(:,2)
-v = matmul((cr_emat), vector)
-vector = eigen_car(:,3)
-w = matmul((cr_emat), vector)
-     cr_prin  (1,1:3, iatom) = eigen_car(:,1)
-     cr_prin  (2,1:3, iatom) = eigen_car(:,2)
-     cr_prin  (3,1:3, iatom) = eigen_car(:,3)
-     cr_prin  (:,4  , iatom) = eigen_val
-!write(*,'(a,3f9.5,a,f9.5,a,f9.5)') 'E(1,0,0) => ', cr_prin(1,1:3,iatom), ' |u|', lib_blen(cr_gten, u), ' VAL ', cr_prin(1,4,iatom)
-!write(*,'(a,3f9.5,a,f9.5,a,f9.5)') 'E(0,1,0) => ', cr_prin(2,1:3,iatom), ' |v|', lib_blen(cr_gten, v), ' VAL ', cr_prin(2,4,iatom)
-!write(*,'(a,3f9.5,a,f9.5,a,f9.5)') 'E(0,0,1) => ', cr_prin(3,1:3,iatom), ' |w|', lib_blen(cr_gten, w), ' VAL ', cr_prin(3,4,iatom)
-!write(*,'(a, f9.5)') ' Ang u,v    ', lib_bang(cr_gten, u,v)
-!write(*,'(a, f9.5)') ' Ang u,w    ', lib_bang(cr_gten, u,w)
-!write(*,'(a, f9.5)') ' Ang v,w    ', lib_bang(cr_gten, v,w)
-!     call test_eigen_value(uij, eigen_val, eigen_cry, cr_gten )
-!write(*,'(a,i4, 6f8.3)') ' ANIS ', iatom, cr_anis_full(1:6,iatom)
-enddo loop_atoms
+!vector = eigen_car(:,1)
+!u = matmul((emat), vector)
+!vector = eigen_car(:,2)
+!v = matmul((emat), vector)
+!vector = eigen_car(:,3)
+!w = matmul((emat), vector)
+!write(*,'(a,3f9.5,a,f9.5,a,f9.5)') 'E(1,0,0) => ', prin(1,1:3,ientry), ' |u|', lib_blen(gten, u), ' VAL ', prin(1,4,ientry)
+!write(*,'(a,3f9.5,a,f9.5,a,f9.5)') 'E(0,1,0) => ', prin(2,1:3,ientry), ' |v|', lib_blen(gten, v), ' VAL ', prin(2,4,ientry)
+!write(*,'(a,3f9.5,a,f9.5,a,f9.5)') 'E(0,0,1) => ', prin(3,1:3,ientry), ' |w|', lib_blen(gten, w), ' VAL ', prin(3,4,ientry)
+!write(*,'(a, f9.5)') ' Ang u,v    ', lib_bang(gten, u,v)
+!write(*,'(a, f9.5)') ' Ang u,w    ', lib_bang(gten, u,w)
+!write(*,'(a, f9.5)') ' Ang v,w    ', lib_bang(gten, v,w)
 !
-cr_nanis = cr_ncatoms
-!
-!
-end subroutine prep_anis
+end subroutine calc_prin_3x3
 !
 !*******************************************************************************
 !
