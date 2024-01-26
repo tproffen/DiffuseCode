9c7287f5d9976acf2be28c3cbdce550ed101b1b3
rneder <reinhard.neder@fau.de>
2024-01-26 10:25:25 +0100

    Added a long version

--- discus/prog/atom_name.f90
+++ discus/prog/atom_name.f90
@@ -45,4 +45,40 @@ END FUNCTION at_name
 !
 !*****7***************************************************************  
 !
+CHARACTER(len=17) function at_name_long(iscat) 
+!+                                                                      
+!     This function builds the atom name as XX(iscat) to be             
+!     able to distinguish between different atom types with             
+!     the same name.                                                    
+!-                                                                      
+USE discus_config_mod 
+USE crystal_mod 
+!
+USE lib_length
+use precision_mod
+use blanks_mod
+!                                                                       
+IMPLICIT none 
+!                                                                       
+integer, dimension(3), intent(in) :: iscat
+CHARACTER(len=PREC_STRING) :: istr 
+INTEGER   :: i, il, is 
+!                                                                       
+!                                                                       
+if(iscat(1)==-1) then
+  at_name_long ='(*)'
+else
+   write(istr, '(a1,3(i5,a1))') '(', iscat(1), ',', iscat(2), ',', iscat(3), ')'
+   is = 19
+   call rem_bl(istr, is)
+!                                                                       
+   il = len_str(cr_at_lis(iscat(1)) ) 
+!                                                                       
+   at_name_long = cr_at_lis(iscat(1)) (1:il) //istr(1:is) 
+endif
+!                                                                       
+END FUNCTION at_name_long
+!
+!*****7***************************************************************  
+!
 END MODULE atom_name
