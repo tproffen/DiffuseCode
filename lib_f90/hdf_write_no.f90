522a698307ec700c5543646e7c41c4602dfcb458
Thomas Proffen <thomasproffen@gmail.com>
2024-02-28 11:04:44 +0100

    Fixed parameter error
    Additional parameters in the HDF version were not added to the non HDF file. Fixed

--- lib_f90/hdf_write_no.f90
+++ lib_f90/hdf_write_no.f90
@@ -5,9 +5,10 @@ contains
 !*****7*****************************************************************
 !
 SUBROUTINE gen_hdf5_write (value, laver, outfile, out_inc, out_eck, out_vi, &
+                       extr_abs, extr_ord, extr_top,                       &
                        cr_a0, cr_win, qvalues, VAL_PDF, VAL_3DPDF, valmax, &
                        ier_num, ier_typ, ER_IO, ER_APPL)
-!
+
 !USE hdf5
 !use diffuse_mod
 !use fourier_sup
@@ -23,6 +24,9 @@ CHARACTER(LEN=200), INTENT(IN) :: outfile
 INTEGER, DIMENSION(3)  , INTENT(IN) :: out_inc
 REAL(kind=PREC_DP)   , DIMENSION(3,4), INTENT(IN) :: out_eck ! (3,4)
 REAL(kind=PREC_DP)   , DIMENSION(3,3), INTENT(IN) :: out_vi
+integer                              , intent(in) :: extr_abs
+integer                              , intent(in) :: extr_ord
+integer                              , intent(in) :: extr_top
 REAL(kind=PREC_DP)   , DIMENSION(3)  , INTENT(IN) :: cr_a0
 REAL(kind=PREC_DP)   , DIMENSION(3)  , INTENT(IN) :: cr_win
 REAL(kind=PREC_DP)   , DIMENSION(out_inc(1), out_inc(2), out_inc(3)), INTENT(IN) :: qvalues
