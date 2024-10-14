a793cb5b3bf22e8d0899dd70461f0b10eb339879
rneder <reinhard.neder@fau.de>
2024-10-14 13:09:39 +0200

    Corrected calculation of mass for atoms with equivalent scattering factor

--- discus/prog/discus_crystal.f90
+++ discus/prog/discus_crystal.f90
@@ -18,6 +18,7 @@ USE precision_mod
 !
 IMPLICIT NONE
 !
+character(len=4) :: at_name  ! Atom name
 INTEGER :: iscat
 INTEGER :: i
 !
@@ -33,7 +34,13 @@ ENDDO
 cr_nreal = SUM(NINT(cr_niscat(1:cr_nscat)*cr_occ(1:cr_nscat)))  ! Add real atom numbers
 !
 DO iscat=1,cr_nscat
-   cr_mass = cr_mass + cr_niscat(iscat)*cr_occ(iscat)*get_mass(cr_at_lis(iscat))
+   if(cr_scat_equ(iscat)) then      ! Equivalent atom name exists
+      at_name = cr_at_equ(iscat)    ! Use the corresponding equivalen atom name
+   else
+      at_name = cr_at_lis(iscat)    ! Use regular atom name
+   endif
+   cr_mass = cr_mass + cr_niscat(iscat)*cr_occ(iscat)*get_mass(at_name)
+!   cr_mass = cr_mass + cr_niscat(iscat)*cr_occ(iscat)*get_mass(cr_at_lis(iscat))
 ENDDO
 !
 END SUBROUTINE crystal_calc_mass
