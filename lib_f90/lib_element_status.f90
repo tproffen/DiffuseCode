ee6bf65894f4f8cc29ef75f3d3f06da066d2790b
rneder <reinhard.neder@fau.de>
2024-10-14 09:10:26 +0200

    New module with status flags

---
+++ lib_f90/lib_element_status.f90
@@ -0,0 +1,12 @@
+module lib_element_status_mod
+!-
+!  Status for the guess work on element nacmes
+!+
+!
+integer, parameter :: IS_FAIL       = -1
+integer, parameter :: IS_NEUTRAL    =  1
+integer, parameter :: IS_ION        =  2
+integer, parameter :: IS_EQUIVALENT =  3
+integer, parameter :: IS_GUESS      =  4
+!
+end module lib_element_status_mod
