4fed4fe0341ac69e934b71f4044cf99cd71e5331
rneder <reinhard.neder@fau.de>
2024-11-16 18:08:23 +0100

    Added cleaner 'reset data' command

--- kuplot/prog/kuplot_reset.f90
+++ kuplot/prog/kuplot_reset.f90
@@ -19,7 +19,6 @@ SUBROUTINE kuplot_do_reset(zeile, lp)
 USE str_comp_mod
       USE take_param_mod
 use kuplot_blk_mod
-use lib_data_struc_h5
 !                                                                       
       IMPLICIT none 
 !                                                                       
@@ -34,29 +33,30 @@ use lib_data_struc_h5
 !                                                                       
 !------ get parameters                                                  
 !                                                                       
-      CALL get_params (zeile, ianz, cpara, lpara, maxw, lp) 
-      IF (ier_num.ne.0) return 
+CALL get_params (zeile, ianz, cpara, lpara, maxw, lp) 
+IF (ier_num.ne.0) return 
 !                                                                       
 !------ No parameters                                                   
 !                                                                       
-      IF (ianz.eq.0) then 
-         WRITE (output_io, 1000) 
-         iz = 1 
-         iframe = 1 
+!
+IF (ianz.eq.0) then 
+   WRITE (output_io, 1000) 
+   iz = 1 
+   iframe = 1 
 !                                                                       
-         DO i = 1, maxkurvtot 
-         lni (i) = .false. 
-         ikfirst (i) = .true. 
-         ENDDO 
+   DO i = 1, maxkurvtot 
+      lni (i) = .false. 
+      ikfirst (i) = .true. 
+   ENDDO 
 !                                                                       
-         DO iw = 1, maxwin 
-         iaf (iw) = 1 
-         frame (iw, 1, 1) = 0.0 
-         frame (iw, 1, 2) = 0.0 
-         frame (iw, 1, 3) = 1.0 
-         frame (iw, 1, 4) = 1.0 
+   DO iw = 1, maxwin 
+      iaf (iw) = 1 
+      frame (iw, 1, 1) = 0.0 
+      frame (iw, 1, 2) = 0.0 
+      frame (iw, 1, 3) = 1.0 
+      frame (iw, 1, 4) = 1.0 
 !                                                                       
-         DO i = 1, maxframe 
+      DO i = 1, maxframe 
          ex (iw, i, 1) = - 9999. 
          ey (iw, i, 1) = - 9999. 
          lyskal (iw, i) = .false. 
@@ -69,46 +69,66 @@ use lib_data_struc_h5
          shear (iw, i) = 90.0 
 !                                                                       
          DO j = 1, maxkurvtot 
-         infra (iw, i, j) = j 
+            infra (iw, i, j) = j 
          ENDDO 
-         ENDDO 
-         ENDDO 
+      ENDDO 
+   ENDDO 
+   call kuplot_reset_data
 !                                                                       
 !------ Subcommand                                                      
 !                                                                       
-      ELSE 
-         IF (str_comp (cpara (1) , 'all', 1, lpara (1) , 3) ) then 
-            WRITE (output_io, 1100) 
-            CALL kuplot_initarrays 
-         ELSE 
-            ier_num = - 6 
-            ier_typ = ER_APPL 
-         ENDIF 
-      ENDIF 
+ELSE 
+   IF(str_comp(cpara(1), 'all', 1, lpara(1), 3)) then 
+      WRITE (output_io, 1100) 
+      CALL kuplot_initarrays 
+      call kuplot_reset_data
+   elseif(str_comp(cpara(1), 'data', 1, lpara(1), 3)) then 
+      call kuplot_reset_data
+   ELSE 
+      ier_num = - 6 
+      ier_typ = ER_APPL 
+   ENDIF 
+ENDIF 
 !
-dx    = 0.0
-dy    = 0.0
-x     = 0.0
-y     = 0.0
-xmin  = 0.0
-xmax  = 0.0
-ymin  = 0.0
-ymax  = 0.0
-z     = 0.0
+!                                                                       
+ 1000 FORMAT     (1x,'Resetting ..') 
+ 1100 FORMAT     (1x,'Resetting - all parameters ..') 
+      END SUBROUTINE kuplot_do_reset                       
+!
+!****7******************************************************************
+!
+subroutine kuplot_reset_data
+!-
+! Set data set number back to zero initialize corresponding arrays
+!+
+use kuplot_mod 
+!
+use lib_data_struc_h5
+use precision_mod
+!
+iz = 1
+lh5   = .false.
+lni   = .false.
+ikfirst = .true.
+ku_ndims = 1
+dx    = 0.0_PREC_DP
+dy    = 0.0_PREC_DP
+x     = 0.0_PREC_DP
+y     = 0.0_PREC_DP
+xmin  = 0.0_PREC_DP
+xmax  = 0.0_PREC_DP
+ymin  = 0.0_PREC_DP
+ymax  = 0.0_PREC_DP
+z     = 0.0_PREC_DP
 lenc  = 0
 offxy = 0
 offz  = 0
 fname = ' '
 fform = ' '
-lh5   = .false.
-lni   = .false.
-ku_ndims = 1
 !
-CALL dgl5_reset
-!                                                                       
- 1000 FORMAT     (1x,'Resetting ..') 
- 1100 FORMAT     (1x,'Resetting - all parameters ..') 
-      END SUBROUTINE kuplot_do_reset                       
+call dgl5_reset
+!
+end subroutine kuplot_reset_data
 !
 !****7******************************************************************
 !
