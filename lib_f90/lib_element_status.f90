ec19859f5ffae5c7ff8ccd9adee261c6a53b8cd9
rneder <reinhard.neder@fau.de>
2024-11-16 18:03:01 +0100

    Improved Element type guesswork

--- lib_f90/lib_element_status.f90
+++ lib_f90/lib_element_status.f90
@@ -8,5 +8,7 @@ integer, parameter :: IS_NEUTRAL    =  1
 integer, parameter :: IS_ION        =  2
 integer, parameter :: IS_EQUIVALENT =  3
 integer, parameter :: IS_GUESS      =  4
+integer, parameter :: IS_GUESS_2    =  5
+integer, parameter :: IS_GUESS_1    =  6
 !
 end module lib_element_status_mod
