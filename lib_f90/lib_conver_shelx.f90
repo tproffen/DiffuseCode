8f2507fb531d98800ee81849929022c788494ba2
rneder <reinhard.neder@fau.de>
2024-12-04 08:21:48 +0100

    Added missing call to LIST 2/4

--- lib_f90/lib_conver_shelx.f90
+++ lib_f90/lib_conver_shelx.f90
@@ -6,7 +6,7 @@ contains
 !
 !*******************************************************************************
 !
-subroutine lib_convert_shelx(infile, outfile)
+subroutine lib_convert_shelx(infile, outfile, hkl_max)
 !
 ! Main interface, receives file
 ! Calls specific converter routines
@@ -19,6 +19,7 @@ implicit none
 !
 character(len=*), intent(in) :: infile
 character(len=*), intent(in) :: outfile   ! output file
+integer, dimension(3), intent(out) :: hkl_max   ! maximum abs(h,k,l)
 !
 integer, parameter :: IRD = 45
 integer, parameter :: IWR = 46
@@ -67,14 +68,15 @@ endif
 !
 if(initial(2)(1:56)=='# h,k,l, Fo-squared, sigma(Fo-squared), Fc and phi(calc)') then
    fcf_type = 6
-   call lib_convert_shelx_fcf_6(IRD, IWR, initial)
+   call lib_convert_shelx_fcf_6(IRD, IWR, initial,hkl_max)
 elseif(initial(2)(1:66)=='# h,k,l, Fc-squared, Fo-squared, sigma(Fo-squared) and status flag') then
    fcf_type = 4
+   call lib_convert_shelx_fcf_4(IRD, IWR, initial, hkl_max)
 elseif(initial(2)(1:47)=='# Unique observed reflections after correcting') then
    fcf_type = 3
 else
    fcf_type = 2
-   call lib_convert_shelx_fcf_2(IRD, IWR, initial)
+   call lib_convert_shelx_fcf_2(IRD, IWR, initial, hkl_max)
    if(ier_num/=0) then
       ier_msg(1) = infile((max(1,len_trim(infile)-39)):len_trim(infile))
       close(IRD)
@@ -89,7 +91,7 @@ end subroutine lib_convert_shelx
 !
 !*******************************************************************************
 !
-subroutine lib_convert_shelx_fcf_6(IRD, IWR, initial)
+subroutine lib_convert_shelx_fcf_6(IRD, IWR, initial, hkl_max)
 !-
 !  Convert a SHELX fcf LIST 6 file
 !
@@ -101,6 +103,7 @@ implicit none
 integer, intent(in) :: IRD
 integer, intent(in) :: IWR
 character(len=*), dimension(2), intent(in) :: initial   ! first two lines
+integer, dimension(3), intent(out) :: hkl_max   ! maximum abs(h,k,l)
 !
 character(len=PREC_STRING)       :: line     !Input lines
 integer                          :: ios       ! input error signal
@@ -112,18 +115,22 @@ loop_header: do
    if(line(1:18) == ' _refln_phase_calc') exit loop_header
 enddo loop_header
 !
+hkl_max = 0
 loop_main: do
    read(IRD,'(a)', iostat=ios) line
    if(is_iostat_end(ios)) exit loop_main
    read(line,*, iostat=ios) hkl, fobs
    write(IWR,'(3i4,2f8.2)') hkl, fobs
+   hkl_max(1) = max(hkl_max(1), abs(hkl(1)))
+   hkl_max(2) = max(hkl_max(2), abs(hkl(2)))
+   hkl_max(3) = max(hkl_max(3), abs(hkl(3)))
 enddo loop_main 
 !
 end subroutine lib_convert_shelx_fcf_6
 !
 !*******************************************************************************
 !
-subroutine lib_convert_shelx_fcf_4(IRD, IWR, initial)
+subroutine lib_convert_shelx_fcf_4(IRD, IWR, initial, hkl_max)
 !-
 !  Convert a SHELX fcf LIST 4 file
 !
@@ -140,6 +147,7 @@ character(len=PREC_STRING)       :: line     !Input lines
 integer                          :: ios       ! input error signal
 integer, dimension(3)            :: hkl       ! Miller indices
 real(kind=PREC_DP), dimension(3) :: fobs      ! F(obs) sigma(Fobs)
+integer, dimension(3), intent(out) :: hkl_max   ! maximum abs(h,k,l)
 !
 loop_header: do
    read(IRD,'(a)') line
@@ -151,13 +159,16 @@ loop_main: do
    if(is_iostat_end(ios)) exit loop_main
    read(line,*, iostat=ios) hkl, fobs
    write(IWR,'(3i4,2f8.2)') hkl, fobs(2:3)
+   hkl_max(1) = max(hkl_max(1), abs(hkl(1)))
+   hkl_max(2) = max(hkl_max(2), abs(hkl(2)))
+   hkl_max(3) = max(hkl_max(3), abs(hkl(3)))
 enddo loop_main 
 !
 end subroutine lib_convert_shelx_fcf_4
 !
 !*******************************************************************************
 !
-subroutine lib_convert_shelx_fcf_2(IRD, IWR, initial)
+subroutine lib_convert_shelx_fcf_2(IRD, IWR, initial, hkl_max)
 !-
 !  Convert a SHELX fcf LIST 2 file
 !
@@ -169,6 +180,7 @@ implicit none
 integer, intent(in) :: IRD
 integer, intent(in) :: IWR
 character(len=*), dimension(2), intent(in) :: initial   ! first two lines
+integer, dimension(3), intent(out) :: hkl_max   ! maximum abs(h,k,l)
 !
 integer                          :: ios       ! input error signal
 integer, dimension(3)            :: hkl       ! Miller indices
@@ -191,6 +203,9 @@ loop_main: do
    if(is_iostat_end(ios)) exit loop_main
    fobs(2) = abs(2.0_PREC_DP*fobs(1)*fobs(2))   ! sigma(fobs) => sigma(I)
    write(IWR,'(3i4,2f8.2,i4)') hkl, fobs
+   hkl_max(1) = max(hkl_max(1), abs(hkl(1)))
+   hkl_max(2) = max(hkl_max(2), abs(hkl(2)))
+   hkl_max(3) = max(hkl_max(3), abs(hkl(3)))
 enddo loop_main 
 !
 end subroutine lib_convert_shelx_fcf_2
