c08d85f277fe7278631b2cb78b6876858c4818c6
rneder <reinhard.neder@fau.de>
2025-09-18 13:52:10 +0200

    Improved boundary checks

--- lib_f90/str_comp_mod.f90
+++ lib_f90/str_comp_mod.f90
@@ -17,13 +17,13 @@ INTEGER         , INTENT(IN) :: j, la, lb
 !
 !character(len=PREC_STRING) :: aa
 !character(len=PREC_STRING) :: bb
-INTEGER i, ia, ib 
+INTEGER :: i, ia, ib 
 !                                                                       
 IF (la == 0 .OR. lb == 0) THEN 
    str_comp = .false. 
 ELSE 
-   ia = MIN (INDEX (a, ' ') , la) 
-   ib = MIN (INDEX (b, ' ') , lb) 
+   ia = MIN (INDEX (a, ' ') , la, len(a)) 
+   ib = MIN (INDEX (b, ' ') , lb, len(b)) 
    IF (ia == 0) THEN 
       ia = la 
    ENDIF 
@@ -41,9 +41,10 @@ ELSE
 !   bb = b(1:len_trim(b))
 !   i  = max(len_trim(aa), len_trim(bb))
 !if(bb(1:3)=='var') then
-!write(*,*) ' AA >',aa(1:len_trim(aa)),'<>', i, ' >>',aa(1:i),'<<'
-!write(*,*) ' BB >',bb(1:len_trim(bb)),'<>', j, ' >>',bb(1:i),'<<'
-!write(*,*) ' ==  ', aa(1:i) .eq.bb(1:i)
+!if(i>len(b)) then
+!write(*,*) ' AA >',a(1:len_trim(a)),'<>', i, ' >>',a,'<<', len(a), la
+!write(*,*) ' BB >',b(1:len_trim(b)),'<>', j, ' >>',b,'<<', len(b), lb, i>len(b)
+!write(*,*) ' ==  ', a(1:i) .eq.b(1:i)
 !endif
    IF (i <  j) THEN 
       str_comp = .false. 
