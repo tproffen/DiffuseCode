51ae6e328bbd31877541b8cb693de6de777018d3
rneder <reinhard.neder@fau.de>
2025-09-24 21:30:00 +0200

    Removed obsolete standalone

--- lib_f90/op_windows.f90
+++ lib_f90/op_windows.f90
@@ -43,14 +43,12 @@ USE prompt_mod
 IMPLICIT NONE
 !
 ! Currently no need for specific exit
-!IF(.NOT. lstandalone .OR. pname_CAP == 'KUPLOT') THEN
 !   WRITE(*,*) ' '
 !   WRITE(*,*) pname_cap,' is finished'
 !   WRITE(*,*) 'Close PGPLOT window '
 !   WRITE(*,*) 'If this is the primary window close pgplot_server as well'
 !   WRITE(*,*) 'Finally close this window as well'
 !   WRITE(*,*) ' '
-!ENDIF
 !
 END SUBROUTINE operating_exit
 END MODULE operating_mod
