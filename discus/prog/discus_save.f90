bd794954dacd4d6cf1c1e4044696f57c93dee1c5
rneder <reinhard.neder@fau.de>
2025-11-11 18:42:54 +0100

    Ongoing UNIFIED structure save

--- discus/prog/discus_save.f90
+++ discus/prog/discus_save.f90
@@ -317,13 +317,13 @@ loop_main: DO while (.not.lend)
       if(ier_num/=0) cycle loop_main
 !
       if(lpresent(O_FORMAT)) then             ! User provided format:
-         if(str_comp(opara(O_FORMAT), 'keyword', 1, lpara(O_FORMAT), 7)) then
+         if(str_comp(opara(O_FORMAT), 'keyword', 1, lopara(O_FORMAT), 7)) then
             sav_keyword = .true.
             is_style = 0
          elseif(str_comp(opara(O_FORMAT), 'nexus', 3, lopara(O_FORMAT), 5)) then
             sav_keyword = .true.
             is_style = 1
-         elseif(str_comp(opara(O_FORMAT), 'nokeyword', 3, lpara(O_FORMAT), 9)) then
+         elseif(str_comp(opara(O_FORMAT), 'nokeyword', 3, lopara(O_FORMAT), 9)) then
             sav_keyword = .false.
             is_style = 2
          endif
@@ -397,6 +397,7 @@ loop_main: DO while (.not.lend)
                         sav_w_mole = .false. 
                         sav_w_doma = .false. 
                         sav_w_prop = .false. 
+                        sav_w_aver = .false. 
                      ELSEIF(str_comp(cpara(1), 'generator', 1, lpara(1), 9) ) THEN
                         sav_w_gene = .false. 
                      ELSEIF(str_comp(cpara(1), 'molecules', 1, lpara(1), 9) ) THEN
@@ -421,6 +422,8 @@ loop_main: DO while (.not.lend)
                         sav_w_magn = .false. 
                      ELSEIF(str_comp(cpara(1), 'property', 1, lpara(1), 8) ) THEN
                         sav_w_prop = .false. 
+                     ELSEIF(str_comp(cpara(1), 'average', 1, lpara(1), 7) ) THEN
+                        sav_w_aver = .false. 
                      ELSE 
                         ier_num = - 6 
                         ier_typ = ER_COMM 
@@ -458,7 +461,8 @@ loop_main: DO while (.not.lend)
             elseif(is_style==0) then
                CALL save_keyword (sav_file) 
             elseif(is_style==1) then
-               CALL save_keyword_nexus(sav_file) 
+               CALL save_keyword_unified(sav_file) 
+!               CALL save_keyword_nexus(sav_file) 
             ELSE 
                CALL save_keyword (sav_file) 
             ENDIF 
@@ -521,6 +525,7 @@ loop_main: DO while (.not.lend)
                         sav_w_obje = .true. 
                         sav_w_doma = .true. 
                         sav_w_prop = .true. 
+                        sav_w_aver = .true. 
                      ELSEIF(str_comp(cpara (1), 'generator', 1, lpara(1), 9) ) THEN
                         sav_w_gene = .true. 
                      ELSEIF(str_comp(cpara (1), 'molecules', 1, lpara(1), 9) ) THEN
@@ -545,6 +550,8 @@ loop_main: DO while (.not.lend)
                         sav_w_magn = .FALSE.    ! MAGNETIC_WORK
                      ELSEIF(str_comp(cpara (1), 'property',  1, lpara(1), 8) ) THEN
                         sav_w_prop = .true. 
+                     ELSEIF(str_comp(cpara (1), 'average',   1, lpara(1), 7) ) THEN
+                        sav_w_aver = .true. 
                      ELSE 
                         ier_num = - 6 
                         ier_typ = ER_COMM 
@@ -1053,9 +1060,302 @@ END SUBROUTINE save_keyword
 !
 !********************************************************************** 
 !
+subroutine save_keyword_unified(outfile) 
+!-
+! Interface to the generic NeXuS nx_write_structure
+!+
+!
+use celltoindex_mod
+use chem_mod
+use chem_aver_mod
+use crystal_mod
+use molecule_mod
+use discus_save_mod
+use wyckoff_mod
+!
+use element_data_mod
+use lib_unified_chars_mod
+use lib_nx_transfer_mod
+use param_mod
+use unified_write_mod
+!
+use errlist_mod
+use envir_mod
+use precision_mod
+use wink_mod
+!
+implicit none
+!
+character(len=*), intent(in) :: outfile
+!
+logical, parameter :: loutput = .false.
+logical, parameter :: lsite   = .true.
+!
+real(KIND=PREC_DP), PARAMETER :: TOL = 1.0D-6
+!
+!character(len=PREC_STRING) :: program_version   ! Which main program wrote the structure
+!character(len=PREC_STRING) :: author_name       ! Authors
+!
+real(kind=PREC_DP), dimension(3)   :: unit_cell_lengths    ! (a, b, c)
+real(kind=PREC_DP), dimension(3)   :: unit_cell_angles     ! (alpha, beta, gamma)
+!
+character(len=32)          :: symmetry_H_M      ! Hermann-Mauguin Symbol
+integer                    :: symmetry_origin   ! Origin choice 1 or 2
+character(len=3)           :: symmetry_abc      ! For orthorhombic space groups abc, cba etc
+integer                    :: symmetry_n_mat    ! Number of symmetry metrices
+real(kind=PREC_DP), dimension(:,:,:), allocatable :: symmetry_mat         ! Direct space symmetry matrices
+!
+integer           , dimension(3)                :: unit_cells   ! Number of unit cells along a, b, c  !DISCUSSION
+!
+integer                                         :: number_of_types     ! Number of atom types
+character(len=4)  , dimension(:),   allocatable :: types_names         ! Atom type names as "O"; "O2-"
+integer           , dimension(:),   allocatable :: types_ordinal       ! Ordinal numbers in Periodic table
+integer           , dimension(:),   allocatable :: types_charge        ! Atomic charge as +2 or -2 ...
+integer           , dimension(:),   allocatable :: types_isotope       ! Zero or specific isotpoe number
+real(kind=PREC_DP), dimension(:),   allocatable :: types_occupancy     ! Occupancy for the atom type, not for an individual atom
+!
+integer                                         :: number_of_atoms  ! Number of atoms
+real(kind=PREC_DP), dimension(:,:), allocatable :: atom_pos         ! Fractional coordinates 1.0 is a subcell unit cell length
+integer           , dimension(:)  , allocatable :: atom_type        ! This atom corresponds to types_names(i)
+integer           , dimension(:,:), allocatable :: atom_unit_cell   ! Atom n is in unit cell (1:3, n)
+integer           , dimension(:)  , allocatable :: atom_site        ! Atom n is on this site in its unit cell
+integer           , dimension(:)  , allocatable :: atom_property    ! The atom has these properties (bits in  atom_property)
+!
+real(kind=PREC_DP), dimension(:,:), allocatable :: magnetic_spins   ! The atom has these magnetic spins
+!
+character(len=PREC_STRING), dimension(  5)      :: crystal_meta     ! Dictionary type, version, date, creation_program, author
+!
+
+type(anis_adp_type)     :: anisotropic_adp      ! Details in lib_nx_transfer_mod.f90
+type(molecule_data)     :: molecules            ! Details in lib_nx_transfer_mod.f90
+type(average_structure) :: average_struc        ! Details in lib_nx_transfer_mod.f90
+logical, dimension(2,6) :: crystal_flags        ! is_super_structure, is_asymmetric_unit, is periodic_x,y,z, is_homogeneous
+logical, dimension(8) :: optional_intended
+!
+character(len=4)      :: element   ! Proper element name
+integer, dimension(3) :: icell     ! Atom is in this unit cell
+integer               :: isite     ! Atom is on this site
+integer :: i,j   ! Dummy counter
+integer :: n_void   ! Structure contains atoms 'VOID' with iscat ==0
+integer :: anis_n  ! Number of anisotropic types
+!
+character(len=8)   :: date
+integer, parameter :: NMSG = ubound(ier_msg,1)
+!
+optional_intended = .false.             ! Default to no flags
+!
+call date_and_time(date=date)
+crystal_meta(1) = 'Disorder structure'
+crystal_meta(2) = '0.0.0'
+crystal_meta(3) = date(1:4) // '-' // date(5:6) // '-' // date(7:8)
+crystal_meta(4) = version_discus
+crystal_meta(5) = author
+optional_intended(o_crystal_meta) = .true.
+!
+unit_cell_lengths  = cr_a0
+unit_cell_angles   = cr_win
+!
+symmetry_H_M       = cr_spcgr
+symmetry_origin    = 1   ! WORK
+symmetry_abc       = cr_set
+symmetry_n_mat     = spc_n
+allocate(symmetry_mat(3,4,symmetry_n_mat))
+symmetry_mat(1:3, 1:4, 1:symmetry_n_mat)   = spc_mat(1:3, 1:4, 1:spc_n)
+! 
+unit_cells         = cr_icc
+!
+call chem_elem(.false.)
+if(nint(res_para(1))>0) then
+   n_void = 1
+else
+   n_void = 0
+endif
+number_of_types    = cr_nscat
+allocate(types_names    (number_of_types+n_void))
+allocate(types_ordinal  (number_of_types+n_void))
+allocate(types_charge   (number_of_types+n_void))
+allocate(types_isotope  (number_of_types+n_void))
+allocate(types_occupancy(number_of_types+n_void))
+!
+do i=1, number_of_types
+   types_names(i)    = cr_at_lis(i)
+   call guess_element(element, j, cr_at_lis(i))
+   if(j<0) then
+      ier_typ = ER_APPL
+      ier_num = -204
+      ier_msg(1) = 'Unknown element : ' // element
+      return
+   endif
+   types_ordinal(i)  = get_ordi(element)
+   j = len_trim(cr_at_lis(i))
+   if(cr_at_lis(i)(j:j)=='+' .or. &
+      cr_at_lis(i)(j:j)=='-' ) then
+      read(cr_at_lis(i)(j-1:j-1),'(i1)') types_charge(i)
+   endif
+   types_isotope(i) = 0
+   types_occupancy(i) = cr_occ(i)
+enddo
+!
+if(n_void==1) then
+   number_of_types = number_of_types + 1
+   types_names    (number_of_types) = 'VOID'
+   types_ordinal  (number_of_types) = 0
+   types_charge   (number_of_types) = 0
+   types_isotope  (number_of_types) = 0
+   types_occupancy(number_of_types) = 1.00_PREC_DP
+endif
+optional_intended(o_types_occupancy) = sav_w_occ
+!
+number_of_atoms = cr_natoms
+allocate(atom_type     (  number_of_atoms))
+allocate(atom_pos      (3,number_of_atoms))
+allocate(atom_unit_cell(3,number_of_atoms))
+allocate(atom_site     (  number_of_atoms))
+allocate(atom_property (  number_of_atoms))
+!
+do i=1, number_of_atoms
+   call indextocell(i, icell, isite)
+   atom_type     (  i) = cr_iscat(1,i)
+   atom_pos      (:,i) = cr_pos(:,i)
+   atom_unit_cell(:,i) = icell
+   atom_site     (  i) = isite
+   atom_property (  i) = cr_prop(i)
+enddo
+optional_intended(o_atom_property) = sav_w_prop
+!
+optional_intended(o_crystal_flags) = .true.
+crystal_flags = .false.
+if(any(cr_icc>1) .and. all(chem_period)) then
+   crystal_flags(:,1) = .true.         !(1,*) value; (2,*) is present and set
+elseif(all(cr_icc==1) .and. cr_natoms== as_natoms) then
+   crystal_flags(1,1) = .false.        ! Is asymmetric
+   crystal_flags(2,1) = .false.        ! Flag is set
+endif
+if(chem_period(1)) crystal_flags(1,3) = .true.
+if(chem_period(2)) crystal_flags(1,4) = .true.
+if(chem_period(3)) crystal_flags(1,5) = .true.
+crystal_flags(2,3:5) = .true.          ! Flags for periodic boundary conditions are set
+!
+crystal_flags(:,6) = cr_flags(:,6)    ! statement on homogeneity   !NEXUS WORK
+!
+!   ! Optional anisotropic atomic displacement parameters Uij
+!
+write(*,*) ' cr_anis ', cr_nanis, lbound(cr_anis_full), ubound(cr_anis_full)
+if(cr_nanis>0) then                    ! Anisotropic ADPs  (also for Uiso
+   anis_n = cr_nanis
+   allocate(anisotropic_adp%anisotropic_adp(7, anis_n))
+   allocate(anisotropic_adp%anisotropic_is_iso(anis_n))
+   allocate(anisotropic_adp%atom_index(number_of_atoms))
+   anisotropic_adp%anisotropic_n_type = anis_n
+   anisotropic_adp%anisotropic_n_atom = number_of_atoms
+   anisotropic_adp%anisotropic_adp    = 0.0_PREC_DP   ! Default
+   anisotropic_adp%anisotropic_adp(1:6, 1:anis_n) = cr_anis_full(1:6, 1:anis_n)
+   do i=1, anis_n
+      if(all(cr_anis_full(2:6,i)<TOL)) then      ! Isotropic only
+         anisotropic_adp%anisotropic_is_iso = 0
+      else
+         anisotropic_adp%anisotropic_is_iso = 1
+      endif
+      anisotropic_adp%anisotropic_adp(7  ,i) = (cr_prin(4,1,i)+cr_prin(4,2,i)+cr_prin(4,3,i))/3.0_PREC_DP
+   enddo
+   do i=1, number_of_atoms
+      anisotropic_adp%atom_index(i)  = cr_iscat(3,i)
+   enddo
+   optional_intended(o_anisotropic_adp) = .true.
+!write(*,*) ' ADP BOUND ', ubound(anisotropic_adp%anisotropic_adp)
+!write(*,'(a,12i4)')           ' ADP index ', anisotropic_adp%atom_index(1:12)
+!write(*,'(a,12i4)')           ' ADP number', anisotropic_adp%anisotropic_n_type, cr_nanis
+!do i=1, cr_nanis
+!write(*,'(a,6f10.6,a,f10.6)') ' ADP uij   ', anisotropic_adp%anisotropic_adp(1:6,i), ' || ',&
+!anisotropic_adp%anisotropic_adp(7,i)
+!enddo
+endif
+optional_intended(o_anisotropic_adp) = sav_w_adp
+!
+! Molecules
+!
+if(mole_num_mole > 0) then
+   molecules%number_moles = mole_num_mole
+   molecules%number_types = mole_num_type
+   allocate(molecules%mole_int  (3, molecules%number_moles))
+   allocate(molecules%mole_real (3, molecules%number_types))
+   j = maxval(mole_len(1:mole_num_mole))
+   allocate(molecules%atom_index(j, molecules%number_moles))
+   molecules%mole_real       = 0.0_PREC_DP
+   do i=1, mole_num_mole
+write(*,*) ' MOLECIULE ', i, mole_type(i), ' >>> ', mole_num_mole, mole_num_type, mole_num_atom
+      molecules%mole_int (1,i)  = mole_type(i)
+      molecules%mole_int (2,i)  = mole_char(mole_type(i))
+      molecules%mole_int (3,i)  = mole_len (i)
+      molecules%mole_real(1,mole_type(i))  = mole_biso(mole_type(i))/(8.*PI**2)
+      molecules%mole_real(2,mole_type(i))  = mole_clin(mole_type(i))
+      molecules%mole_real(3,mole_type(i))  = mole_cqua(mole_type(i))
+      do j=1, mole_len(i)
+         molecules%atom_index(j,i) = mole_cont(mole_off(i)+j)
+      enddo
+   enddo
+   optional_intended(o_molecules) = sav_w_mole
+else
+   optional_intended(o_molecules) = .false.
+endif
+!
+!  Average structure
+!
+if(.not.crystal_flags(1,2) .and.    &  !   Not an asymmetric unit
+        crystal_flags(1,6)      ) then   ! Homogeneous
+   call chem_aver(loutput, lsite)
+   average_struc%aver_n_atoms = cr_ncatoms
+   allocate(average_struc%atom_type  (   average_struc%aver_n_atoms))
+   allocate(average_struc%position   (3, average_struc%aver_n_atoms))
+   allocate(average_struc%occupancy  (   average_struc%aver_n_atoms))
+   allocate(average_struc%anis_adp   (7, average_struc%aver_n_atoms))
+   allocate(average_struc%site_number(   average_struc%aver_n_atoms))
+   do i=1,cr_ncatoms
+      average_struc%atom_type(  i) = chem_ave_iscat(1,i)
+      average_struc%position (:,i) = chem_ave_pos(:,i)
+      average_struc%occupancy(  i) = chem_ave_bese(i,1)   ! SITE IDENTICAL WORK
+      j = chem_ave_anis(i,1)  ! SITE IDENTICAL   WORK
+      average_struc%anis_adp (1:6,i) = cr_anis_full(1:6, j)
+      average_struc%anis_adp (7  ,i) =                                          &
+             (cr_prin(4,   1,j) + cr_prin(4,   2,j) + cr_prin(4,   3,j))/3.0_PREC_DP
+      average_struc%site_number = i
+   enddo
+   optional_intended(o_average_struc) = sav_w_aver
+else
+   optional_intended(o_average_struc) = .false.
+endif
+!
+!  Optional magnetic scattering vectors
+!
+allocate(magnetic_spins(3,number_of_atoms))
+magnetic_spins = 0.0_PREC_DP
+optional_intended(o_magnetic_spins) = sav_w_magn
+!
+!
+call unified_write_structure(outfile, unit_cell_lengths, unit_cell_angles,                &
+                             symmetry_H_M, symmetry_origin, symmetry_abc, symmetry_n_mat, &
+                             symmetry_mat, unit_cells, number_of_types, types_names,      &
+                             types_ordinal, types_charge, types_isotope, number_of_atoms, &
+                             atom_type, atom_pos, atom_unit_cell, atom_site,              &
+                             NMSG, ier_num, ier_msg,                                      &
+                             optional_intended,                                           &
+                             atom_property=atom_property,       &
+                             crystal_flags=crystal_flags,       &
+                             crystal_meta=crystal_meta,         &
+                             anisotropic_adp=anisotropic_adp,   &
+                             molecules=molecules,               &
+                             average_struc=average_struc,       &
+                             magnetic_spins=magnetic_spins,     &
+                             types_occupancy=types_occupancy)
+!
+end subroutine save_keyword_unified
+!
+!********************************************************************** 
+!
 subroutine save_keyword_nexus (outfile) 
 !-
-! Interfact to the generiv NeXuS nx_write_structure
+! Interface to the generic NeXuS nx_write_structure
+! should be obsolete
 !+
 !
 use celltoindex_mod
