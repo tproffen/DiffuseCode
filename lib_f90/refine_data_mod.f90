6d89b9357c7dcb76e8b7dc23f0c84b6e5eacead4
rneder <reinhard.neder@fau.de>
2026-05-06 17:28:04 +0200

    Prep for major update, ongoing

--- lib_f90/refine_data_mod.f90
+++ lib_f90/refine_data_mod.f90
@@ -1,24 +1,35 @@
-MODULE refine_data_mod
+module refine_data_mod
 !
-USE precision_mod
+use lib_data_struc_type_mod
+use precision_mod
 !
-IMPLICIT NONE
+implicit none
 !
-CHARACTER(LEN=PREC_STRING)                                :: ref_load_u  = ' '  ! Load string data
-CHARACTER(LEN=PREC_STRING)                                :: ref_csigma_u = ' ' ! Load string Sigma's
-CHARACTER(LEN=PREC_STRING)                                :: ref_load  = ' '  ! Load string data
-CHARACTER(LEN=PREC_STRING)                                :: ref_csigma = ' ' ! Load string Sigma's
+character(LEN=PREC_STRING)                                :: ref_load_u  = ' '  ! Load string data
+character(LEN=PREC_STRING)                                :: ref_csigma_u = ' ' ! Load string Sigma's
+character(LEN=PREC_STRING)                                :: ref_load  = ' '  ! Load string data
+character(LEN=PREC_STRING)                                :: ref_csigma = ' ' ! Load string Sigma's
+character(len=PREC_STRING), dimension(:)      , allocatable :: ref_data_name    ! Data set names
+integer                   , dimension(:)      , allocatable :: ref_data_number  ! Data set Rnumber in global
 integer                                                   :: ref_ndata = 0    ! Number of data set for REFINE
-INTEGER                                                   :: ref_kload = 0    ! Data set within KUPLOT
-INTEGER                                                   :: ref_ksigma= 0    ! Sigma set within KUPLOT
-INTEGER                                                   :: ref_kupl  = 0    ! Data set within KUPLOT that needs to be kept
-INTEGER                                                   :: ref_type  = 0    ! Data type => lib_data_types_mod
-INTEGER                   , DIMENSION(3,1)                  :: ref_dim    ! Dimensions of data set
-REAL(kind=PREC_DP)        , DIMENSION(:,:,:,:), ALLOCATABLE :: ref_data   ! the actual data set
-REAL(kind=PREC_DP)        , DIMENSION(:,:,:,:), ALLOCATABLE :: ref_sigma  ! sigma at each data point
-REAL(kind=PREC_DP)        , DIMENSION(      :), ALLOCATABLE :: ref_weight ! Weight for each data set, defaults to 1 
-REAL(kind=PREC_DP)        , DIMENSION(:    ,:), ALLOCATABLE :: ref_x      ! x-values of data set
-REAL(kind=PREC_DP)        , DIMENSION(:    ,:), ALLOCATABLE :: ref_y      ! y-values of data set
-REAL(kind=PREC_DP)        , DIMENSION(:    ,:), ALLOCATABLE :: ref_z      ! z-values of data set
+integer                                                   :: ref_maxdata = 0  ! Max Number of data set for REFINE
+integer                                                   :: ref_kload = 0    ! Data set within KUPLOT
+integer                                                   :: ref_ksigma= 0    ! Sigma set within KUPLOT
+integer                                                   :: ref_kupl  = 0    ! Data set within KUPLOT that needs to be kept
+integer                                                   :: ref_type  = 0    ! Data type => lib_data_types_mod
+integer                   , dimension(3,1)                  :: ref_dim    ! Dimensions of data set
+real(kind=PREC_DP)        , dimension(:,:,:,:), allocatable :: ref_data   ! the actual data set
+real(kind=PREC_DP)        , dimension(:,:,:,:), allocatable :: ref_sigma  ! sigma at each data point
+real(kind=PREC_DP)        , dimension(      :), allocatable :: ref_weight ! Weight for each data set, defaults to 1 
+real(kind=PREC_DP)        , dimension(:    ,:), allocatable :: ref_x      ! x-values of data set
+real(kind=PREC_DP)        , dimension(:    ,:), allocatable :: ref_y      ! y-values of data set
+real(kind=PREC_DP)        , dimension(:    ,:), allocatable :: ref_z      ! z-values of data set
 !
-END MODULE refine_data_mod
+type ref_ptr
+   type(h5_data_struc), pointer :: data_ptr
+end type ref_ptr 
+!
+type(ref_ptr), dimension(:), allocatable :: ref_data_ptr
+type(ref_ptr), dimension(:), allocatable :: ref_calc_ptr
+!
+end module refine_data_mod
