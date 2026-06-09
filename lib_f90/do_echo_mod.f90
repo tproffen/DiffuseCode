4c6949528537dedd1fc7dcec9f48fb4fdff8a361
rneder <reinhard.neder@fau.de>
2026-06-09 16:43:38 +0200

    Cosmetics, improved user error handling

--- lib_f90/do_echo_mod.f90
+++ lib_f90/do_echo_mod.f90
@@ -13,142 +13,136 @@ SUBROUTINE echo (zeile, lp)
 !-                                                                      
 !     writes an echo to the screen                                      
 !+                                                                      
-      USE build_name_mod
-      USE errlist_mod 
-      USE get_params_mod
+USE build_name_mod
+USE errlist_mod 
+USE get_params_mod
 USE lib_length
 USE precision_mod
-      USE prompt_mod 
-      IMPLICIT none 
+USE prompt_mod 
 !                                                                       
+IMPLICIT none 
 !                                                                       
-      INTEGER maxp 
-      PARAMETER (maxp = 12) 
-      CHARACTER ( * ) zeile 
-      CHARACTER(LEN=MAX(PREC_STRING, LEN(zeile))) :: string 
-      CHARACTER(LEN=MAX(PREC_STRING, LEN(zeile))) :: cpara (maxp) 
-      CHARACTER(LEN=MAX(PREC_STRING, LEN(zeile))) :: cstr 
-      INTEGER lpara (maxp) 
-      INTEGER ianz 
-      INTEGER lp, i
-      INTEGER iko, iqo, iqo2, lstring 
-      REAL(KIND=PREC_DP), DIMENSION(MAXP) :: werte
-!                                                                       
-!      INTEGER socket_send 
+INTEGER :: maxp 
+!PARAMETER (maxp = 20) 
+CHARACTER(len=*) :: zeile 
+CHARACTER(LEN=MAX(PREC_STRING, LEN(zeile))) :: string 
+!CHARACTER(LEN=MAX(PREC_STRING, LEN(zeile))) :: cstr 
+CHARACTER(LEN=    PREC_STRING             ), dimension(:), allocatable :: cpara !(maxp) 
+INTEGER, dimension(:), allocatable :: lpara !(maxp) 
+INTEGER :: ianz 
+INTEGER :: lp, i
+INTEGER :: iko, iqo, iqo2, lstring 
+REAL(KIND=PREC_DP), DIMENSION(:), allocatable :: werte
 !                                                                       
 !     Find any "" which would mean that we need parameter substitution  
 !                                                                       
-      iqo = index (zeile (1:lp) , '"') 
-      IF (iqo.eq.0) THEN 
+iqo = index (zeile (1:lp) , '"') 
+cond_comma: IF (iqo.eq.0) THEN 
 !                                                                       
 !     --None foud, harmless echo                                        
 !                                                                       
-         WRITE (output_io, 2010) zeile (1:lp) 
-         WRITE (cstr, 2010) zeile (1:lp) 
-!         IF (lconn.and.lsocket) THEN 
-!            il = len_str (cstr) 
-!            ier_num = socket_send (s_conid, cstr, il) 
-!            IF(ier_num < 0) THEN
-!               ier_num = -19
-!               RETURN
-!            ELSE
-!               ier_num = 0
-!            ENDIF
-!         ENDIF 
+   WRITE (output_io, 2010) zeile (1:lp) 
+!   WRITE (cstr, 2010) zeile (1:lp) 
+   return
 !                                                                       
-!        IF (output_status.eq.OUTPUT_FILE) THEN 
-!           WRITE (output_io, 2010) zeile (1:lp) 
-!        ENDIF 
-      ELSE 
+endif  cond_comma
 !                                                                       
+!
+if(allocated(cpara)) deallocate(cpara)
+if(allocated(lpara)) deallocate(lpara)
+if(allocated(werte)) deallocate(werte)
+!
 !     --Look for matching ""                                            
 !                                                                       
-         iqo2 = index (zeile (iqo + 1:lp) , '"') 
-         IF (iqo2.eq.0) THEN 
-            ier_num = -  4 
-            ier_typ = ER_COMM 
-            RETURN 
-         ENDIF 
+iqo2 = index (zeile (iqo + 1:lp) , '"') 
+IF (iqo2.eq.0) THEN 
+   ier_num = -  4 
+   ier_typ = ER_COMM 
+   RETURN 
+ENDIF 
 !                                                                       
 !     --save the string in quotation marks for later use as             
 !       first parameter                                                 
 !                                                                       
-         iqo2 = iqo2 + iqo 
-         iko = index (zeile (iqo2 + 1:lp) , ',') 
-         IF (iko.ne.0) THEN 
-            string = zeile (iqo2 + iko + 1:lp) 
-            lstring = - (lp - iqo2 - iko) 
+iqo2 = iqo2 + iqo 
+iko = index (zeile (iqo2 + 1:lp) , ',') 
+IF (iko.ne.0) THEN 
+   string = zeile (iqo2 + iko + 1:lp) 
+   lstring = - (lp - iqo2 - iko) 
 !                                                                       
 !     --get all other parameters                                        
+!
+   maxp = count_params(string) + 5
+   if(maxp==0) then
+      ier_num = -1
+      ier_typ = ER_COMM
+      return
+   endif
+   allocate(cpara(maxp))
+   allocate(lpara(maxp))
+   allocate(werte(maxp))
+   CALL get_params (string, ianz, cpara, lpara, maxp, lstring) 
+ELSE 
+   maxp = 10
+   allocate(cpara(maxp))
+   allocate(lpara(maxp))
+   allocate(werte(maxp))
+   cpara = ' '
+   lpara = 0
+   werte = 0.0_PREC_DP
+   string = ' ' 
+   lstring = 1 
+   ianz = 0 
+ENDIF 
 !                                                                       
-            CALL get_params (string, ianz, cpara, lpara, maxp, lstring) 
-         ELSE 
-            string = ' ' 
-            lstring = 1 
-            ianz = 0 
-         ENDIF 
-!                                                                       
-         IF (ier_num.eq.0) THEN 
-            DO i = ianz, 1, - 1 
-            cpara (i + 1) = cpara (i) 
-            lpara (i + 1) = lpara (i) 
-            ENDDO 
-            cpara (1) = zeile (1:iqo2) 
-            lpara (1) = iqo2 
-            ianz = ianz + 1 
-            CALL do_build_name (ianz, cpara, lpara, werte, maxp, 1) 
-            IF (ier_num.eq.0) THEN 
+cond_main: IF(ier_num == 0) THEN 
+   DO i = ianz, 1, - 1 
+      cpara(i + 1) = cpara(i) 
+      lpara(i + 1) = lpara(i) 
+   ENDDO 
+   cpara (1) = zeile (1:iqo2) 
+   lpara (1) = iqo2 
+   ianz = ianz + 1 
+   if(ianz>1) then
+      CALL do_build_name (ianz, cpara, lpara, werte, maxp, 1) 
+   else
+      cpara (1) = zeile (2:iqo2-1)
+   endif
+   IF (ier_num /= 0) exit cond_main
 !                                                                       
 !     ------The first comma that follows the quotation marks must be    
 !           omitted, all others retained as part of the echo            
 !                                                                       
-               IF (ianz.eq.1) THEN 
-                  WRITE (output_io, 2000) cpara (ianz) (1:lpara (ianz) ) 
-                  WRITE (cstr, 2000) cpara (ianz) (1:lpara (ianz) ) 
+   IF (ianz.eq.1) THEN 
+      WRITE (output_io, 2000) cpara (ianz) (1:lpara (ianz) ) 
+!      WRITE (cstr, 2000) cpara (ianz) (1:lpara (ianz) ) 
 !                 IF (output_status.eq.OUTPUT_FILE) THEN 
 !                    WRITE (output_io, 2000) cpara (ianz) (1:lpara (    &
 !                    ianz) )                                            
 !                 ENDIF 
-               ELSEIF (ianz.eq.2) THEN 
-                  WRITE (output_io, 2000) cpara (1) (1:lpara (1) ), cpara (    &
-                  ianz) (1:lpara (ianz) )                               
-                  WRITE (cstr, 2000) cpara (1) (1:lpara (1) ), cpara (  &
-                  ianz) (1:lpara (ianz) )                               
-!                 IF (output_status.eq.OUTPUT_FILE) THEN 
-!                    WRITE (output_io, 2000) cpara (1) (1:lpara (1) ),  &
-!                    cpara (ianz) (1:lpara (ianz) )                     
-!                 ENDIF 
-               ELSE 
-                  WRITE (output_io, 2000) cpara (1) (1:lpara (1) ) , (cpara ( &
-                  i) (1:lpara (i) ) , ',', i = 2, ianz - 1) , cpara (   &
-                  ianz) (1:lpara (ianz) )                               
-                  WRITE (cstr, 2000) cpara (1) (1:lpara (1) ) , (cpara (&
-                  i) (1:lpara (i) ) , ',', i = 2, ianz - 1) , cpara (   &
-                  ianz) (1:lpara (ianz) )                               
-!                 IF (output_status.eq.OUTPUT_FILE) THEN 
-!                    WRITE (output_io, 2000) cpara (1) (1:lpara (1) ) , &
-!                    (cpara (i) (1:lpara (i) ) , ',', i = 2, ianz - 1) ,&
-!                    cpara (ianz) (1:lpara (ianz) )                     
-!                 ENDIF 
-               ENDIF 
-!               IF (lconn.and.lsocket) THEN 
-!                  il = len_str (cstr) 
-!                  ier_num = socket_send (s_conid, cstr, il) 
-!                  IF(ier_num < 0) THEN
-!                     ier_num = -19
-!                     RETURN
-!                  ELSE
-!                     ier_num = 0
-!                  ENDIF
-!               ENDIF 
-            ENDIF 
-         ENDIF 
-      ENDIF 
-!                                                                       
+   ELSEIF (ianz.eq.2) THEN 
+      WRITE(output_io, 2000) cpara(1)(1:lpara(1)), cpara(ianz)(1:lpara(ianz))                               
+!      WRITE(cstr, 2000) cpara(1)(1:lpara(1)), cpara(ianz)(1:lpara(ianz))                               
+   ELSE 
+      WRITE(output_io, 2000) cpara(1)(1:lpara(1)),        &
+         (cpara(i)(1:lpara(i)) , ',', i = 2, ianz - 1) ,  &
+          cpara(ianz)(1:lpara(ianz))                               
+!      WRITE(cstr, 2000) cpara(1)(1:lpara(1))  ,           &
+!         (cpara(i)(1:lpara(i)), ',', i = 2, ianz - 1),    &
+!          cpara(ianz)(1:lpara(ianz))                               
+   ENDIF 
+ENDIF cond_main
+!
+if(allocated(cpara)) deallocate(cpara)
+if(allocated(lpara)) deallocate(lpara)
+if(allocated(werte)) deallocate(werte)
+!
  2000 FORMAT    (1x,21a) 
  2010 FORMAT    (1x,  a) 
-!                                                                       
-      END SUBROUTINE echo                           
+!
+END SUBROUTINE echo                           
+!
+!*****7*****************************************************************
 !
 SUBROUTINE do_flush (zeile, lp) 
 !-
